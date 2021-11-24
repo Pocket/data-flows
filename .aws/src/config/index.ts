ca39ac07ffcbf820c32cb3223b0b152e0b55eb5b
Mathijs Miermans <m.miermans@gmail.com>
2021-11-24 13:11:53 -0800

    Initial ECS deployment

---
+++ .aws/src/config/index.ts
@@ -0,0 +1,46 @@
+const name = 'DataFlows';
+const domainPrefix = 'data-flows';
+const isDev = process.env.NODE_ENV === 'development';
+const environment = isDev ? 'Dev' : 'Prod';
+const domain = isDev
+  ? `${domainPrefix}.getpocket.dev`
+  : `${domainPrefix}.readitlater.com`;
+const graphqlVariant = isDev ? 'development' : 'current';
+const githubConnectionArn = isDev
+  ? 'arn:aws:codestar-connections:us-east-1:410318598490:connection/7426c139-1aa0-49e2-aabc-5aef11092032'
+  : 'arn:aws:codestar-connections:us-east-1:996905175585:connection/5fa5aa2b-a2d2-43e3-ab5a-72ececfc1870';
+const branch = isDev ? 'dev' : 'main';
+const prefect = {
+  port: 8080,
+}
+
+export const config = {
+  name,
+  isDev,
+  prefix: `${name}-${environment}`,
+  circleCIPrefix: `/${name}/CircleCI/${environment}`,
+  shortName: 'DATAFL',
+  environment,
+  domain,
+  prefect,
+  codePipeline: {
+    githubConnectionArn,
+    repository: 'pocket/data-flows',
+    branch,
+  },
+  graphqlVariant,
+  healthCheck: {
+    command: [
+      'CMD-SHELL',
+      `curl -f http://localhost:${prefect.port}/api/health || exit 1`,
+    ],
+    interval: 15,
+    retries: 3,
+    timeout: 5,
+    startPeriod: 0,
+  },
+  tags: {
+    service: name,
+    environment,
+  },
+};
---
+++ .aws/src/main.ts
@@ -0,0 +1,237 @@
+import {Construct} from 'constructs';
+import {App, DataTerraformRemoteState, RemoteBackend, TerraformStack,} from 'cdktf';
+import {config} from './config';
+import {PocketALBApplication, PocketECSCodePipeline, PocketPagerDuty,} from '@pocket-tools/terraform-modules';
+
+// Providers
+import {
+  AwsProvider,
+  ELB,
+  ACM,
+  DataSources,
+  KMS,
+  SNS,
+} from '@cdktf/provider-aws';
+
+class DataFlows extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, 'aws', { region: 'us-east-1' });
+
+    new RemoteBackend(this, {
+      hostname: 'app.terraform.io',
+      organization: 'Pocket',
+      workspaces: [{ prefix: `${config.name}-` }],
+    });
+
+    const region = new DataSources.DataAwsRegion(this, 'region');
+    const caller = new DataSources.DataAwsCallerIdentity(this, 'caller');
+    const secretsManager = new KMS.DataAwsKmsAlias(this, 'kms_alias', {
+      name: 'alias/aws/secretsmanager',
+    });
+
+    const pocketApp = this.createPocketAlbApplication({
+      secretsManagerKmsAlias: this.getSecretsManagerKmsAlias(),
+      snsTopic: this.getCodeDeploySnsTopic(),
+      region,
+      caller,
+    });
+
+    this.createApplicationCodePipeline(pocketApp);
+  }
+
+  /**
+   * Get the sns topic for code deploy
+   * @private
+   */
+  private getCodeDeploySnsTopic() {
+    return new SNS.DataAwsSnsTopic(this, 'backend_notifications', {
+      name: `Backend-${config.environment}-ChatBot`,
+    });
+  }
+
+  /**
+   * Get secrets manager kms alias
+   * @private
+   */
+  private getSecretsManagerKmsAlias() {
+    return new KMS.DataAwsKmsAlias(this, 'kms_alias', {
+      name: 'alias/aws/secretsmanager',
+    });
+  }
+
+  /**
+   * Create CodePipeline to build and deploy terraform and ecs
+   * @param app
+   * @private
+   */
+  private createApplicationCodePipeline(app: PocketALBApplication) {
+    new PocketECSCodePipeline(this, 'code-pipeline', {
+      prefix: config.prefix,
+      source: {
+        codeStarConnectionArn: config.codePipeline.githubConnectionArn,
+        repository: config.codePipeline.repository,
+        branchName: config.codePipeline.branch,
+      },
+    });
+  }
+
+  private createPocketAlbApplication(dependencies: {
+    region: DataSources.DataAwsRegion;
+    caller: DataSources.DataAwsCallerIdentity;
+    secretsManagerKmsAlias: KMS.DataAwsKmsAlias;
+    snsTopic: SNS.DataAwsSnsTopic;
+  }): PocketALBApplication {
+    const {
+      region,
+      caller,
+      secretsManagerKmsAlias,
+      snsTopic,
+    } = dependencies;
+
+    const parameter_arn_prefix: string =
+      `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}`
+
+    return new PocketALBApplication(this, 'application', {
+      internal: true,
+      prefix: config.prefix,
+      alb6CharacterPrefix: config.shortName,
+      tags: config.tags,
+      cdn: false,
+      domain: config.domain,
+      containerConfigs: [
+        {
+          name: 'prefect',
+          containerImage: 'prefecthq/prefect:0.15.9-python3.9',
+          portMappings: [
+            {
+              hostPort: config.prefect.port,
+              containerPort: config.prefect.port,
+            },
+          ],
+          command: ["prefect", "agent", "ecs", "start", "--agent-address http://localhost:8080"],
+          healthCheck: config.healthCheck,
+          envVars: [
+            {
+              name: 'PREFECT__CLOUD__API',
+              value: 'https://api.prefect.io',
+            },
+            {
+              name: 'PREFECT__CLOUD__AGENT__LABELS',
+              value: JSON.stringify([config.environment]),
+            },
+            {
+              name: 'PREFECT__CLOUD__AGENT__LEVEL',
+              value: config.isDev ? 'DEBUG' : 'INFO',
+            },
+          ],
+          secretEnvVars: [
+            {
+              name: 'PREFECT__CLOUD__API_KEY',
+              valueFrom: `${parameter_arn_prefix}/PREFECT_API_KEY`,
+            },
+          ],
+        },
+      ],
+      codeDeploy: {
+        useCodeDeploy: true,
+        useCodePipeline: true,
+        snsNotificationTopicArn: snsTopic.arn,
+      },
+      exposedContainer: {
+        name: 'prefect',
+        port: config.prefect.port,
+        healthCheckPath: '/api/health',
+      },
+      ecsIamConfig: {
+        prefix: config.prefix,
+        taskExecutionRolePolicyStatements: [
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
+            resources: [
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
+              secretsManagerKmsAlias.targetKeyArn,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.name}/${config.environment}`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.name}/${config.environment}/*`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.prefix}`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.prefix}/*`,
+            ],
+            effect: 'Allow',
+          },
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['ssm:GetParameter*'],
+            resources: [
+              parameter_arn_prefix,
+              `${parameter_arn_prefix}/*`,
+            ],
+            effect: 'Allow',
+          },
+        ],
+        taskRolePolicyStatements: [
+          // The Prefect ECS Agent will need permissions to create task definitions and start tasks in your ECS Cluster.
+          // @see https://docs.prefect.io/orchestration/agents/ecs.html#execution-role-arn
+          {
+            actions: [
+                'ecs:RunTask',
+                'ecs:StopTask',
+                // 'ecs:CreateCluster',
+                // 'ecs:DeleteCluster',
+                'ecs:RegisterTaskDefinition',
+                'ecs:DeregisterTaskDefinition',
+                'ecs:DescribeClusters',
+                'ecs:DescribeTaskDefinition',
+                'ecs:DescribeTasks',
+                'ecs:ListAccountSettings',
+                'ecs:ListClusters',
+                'ecs:ListTaskDefinitions',
+            ],
+            resources: [
+              `arn:aws:ecs:${region.name}:${caller.accountId}:cluster/${config.prefix}`,
+              `arn:aws:ecs:${region.name}:${caller.accountId}:task/${config.prefix}/*`,
+              `arn:aws:ecs:${region.name}:${caller.accountId}:container/${config.prefix}/*`,
+              `arn:aws:ecs:${region.name}:${caller.accountId}:task-definition/${config.prefix}:*`,
+              `arn:aws:ecs:${region.name}:${caller.accountId}:container/${config.prefix}/*`,
+            ],
+            effect: 'Allow',
+          },
+          // X-Ray permissions
+          {
+            actions: [
+              'xray:PutTraceSegments',
+              'xray:PutTelemetryRecords',
+              'xray:GetSamplingRules',
+              'xray:GetSamplingTargets',
+              'xray:GetSamplingStatisticSummaries',
+            ],
+            resources: ['*'],
+            effect: 'Allow',
+          },
+        ],
+        taskExecutionDefaultAttachmentArn:
+          'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+      },
+      autoscalingConfig: {
+        targetMinCapacity: 2,
+        targetMaxCapacity: 10,
+      },
+      alarms: {
+        //TODO: When you start using the service add the pagerduty arns as an action `pagerDuty.snsNonCriticalAlarmTopic.arn`
+        http5xxErrorPercentage: {
+          threshold: 25,
+          evaluationPeriods: 4,
+          period: 300,
+          actions: config.isDev ? [] : []
+        }
+      },
+    });
+  }
+}
+
+const app = new App();
+new DataFlows(app, 'data-flows');
+// TODO: Fix the terraform version. @See https://github.com/Pocket/related-content-api/pull/333
+app.synth();
