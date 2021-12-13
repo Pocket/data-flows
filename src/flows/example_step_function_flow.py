e05551a7561bd6808794cbab0b6598e66519c731
Mathijs Miermans <m.miermans@gmail.com>
2021-12-13 12:02:08 -0800

    Add basic steps to Readme

---
+++ src/flows/example_step_function_flow.py
@@ -0,0 +1,34 @@
+from os import environ
+
+import prefect
+from prefect import task, Flow
+from prefect.run_configs import ECSRun
+from prefect.tasks.aws.step_function import StepActivate
+
+
+@task
+def abc(data):
+    logger = prefect.context.get("logger")
+    logger.info(f"{data['result']}")
+    return {'result': 'I said Hello'}
+
+
+with Flow("ecs_test") as flow:
+    result = abc({'result': 'hello world'})
+    abc(result)
+
+# TODO: In production, the steps below would be taken by a deployment script. They're just included here as an example.
+flow.storage = prefect.storage.S3(
+    bucket='pocket-dataflows-storage-dev',
+    add_default_labels=False
+)
+
+flow.run_config = ECSRun(
+    # task_definition_path="test.yaml",
+    labels=['Dev'],
+    task_role_arn=environ.get('PREFECT_TASK_ROLE_ARN'),
+    # execution_role_arn='arn:aws:iam::12345678:role/prefect-ecs',
+    image='prefecthq/prefect:latest-python3.9',
+)
+
+flow.register(project_name="prefect-tutorial")
