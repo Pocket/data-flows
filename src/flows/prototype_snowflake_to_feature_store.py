6ce5e6e5cc04645b52458211441da5717a55cbfc
Chelsea Troy <chelsea.dommert@gmail.com>
2021-12-28 12:14:40 -0600

    Move to a secret strategy that allows us to keep config in the repo and depend less on people's local machine settings
    Update README for developers to include all environment variables and secrets as laid out in the previous commit
    
    Remove extraneous example env file
    
    Document how to create and add dataframe data to a feature store from a Prefect task

--- src/flows/prototype_snowflake_to_feature_store.py
+++ src/flows/prototype_snowflake_to_feature_store.py
@@ -1,9 +1,6 @@
-import prefect
-from prefect import task, Flow, Parameter, unmapped
-from prefect.run_configs import ECSRun
-from prefect.tasks.aws.s3 import S3List
-from prefect.tasks.snowflake import SnowflakeQuery
 import pandas as pd
+from prefect import task, Flow
+
 import boto3
 from sagemaker.feature_store.feature_group import FeatureGroup
 from sagemaker.session import Session
@@ -15,11 +12,21 @@ from src.lib.queries import get_snowflake_query
 def extract():
     """
     Pull data from snowflake materialized tables.
+
     Returns:
+
+    A dataframe containing the results of a snowflake query represented as a pandas dataframe
     """
-    return get_snowflake_query().run(query="""
-    select * from ANALYTICS.DBT_GKATRE.PRE_CURATED_READING_METRICS limit 10
+    query_result =  get_snowflake_query().run(query="""
+    select 
+    
+    resolved_id, 
+    resolved_url, 
+    to_varchar(published_at,'yyyy-MM-dd"T"HH:mm:ssZ')::string as published_at
+    
+    from ANALYTICS.DBT.CONTENT limit 10
     """)
+    return pd.DataFrame(query_result, columns=['RESOLVED_ID', 'RESOLVED_URL', 'PUBLISHED_AT'])
 
     # TODO: How to put this in a data frame?
 
@@ -29,28 +36,36 @@ def load(df: pd.DataFrame, feature_group_name):
     Update SageMaker feature group.
 
     Args:
-        df:
-        feature_group_name:
+        df : the data in a dataframe to upload to the feature group
+        feature_group_name: the name of the feature group to upload the data to
 
     Returns:
-
+        EITHER -
+        success case: the success message from the feature group API
+        failure case: a description of the feature group as it currently stands for debugging
     """
     boto_session = boto3.Session()
     feature_store_session = Session(boto_session=boto_session,
                                     sagemaker_client=boto_session.client(service_name='sagemaker'),
                                     sagemaker_featurestore_runtime_client=boto_session.client(service_name='sagemaker-featurestore-runtime'))
     feature_group = FeatureGroup(name=feature_group_name, sagemaker_session=feature_store_session)
-    feature_group.ingest(df, max_workers=4, max_processes=4, wait=True)
+    try:
+        result = feature_group.ingest(data_frame=df, max_workers=4, max_processes=4, wait=True)
+    except:
+        # This assumes that the feature group already exists.
+        # If it doesn't, you'll need to create it in AWS. We have a notebook you can repurpose CreateFeatureGroupForPrefect.ipynb in Chelsea's Sagemaker Studio in dev
+        result = feature_group.describe()
+    return result
 
 @task
 def print_results(results):
     print(results)
 
-
 with Flow("User Impression Feature Group Flow") as flow:
     df = extract()
     print_results(df)
-    # load(df, 'some_name')
+    result = load(df, 'test-prefect')
+    print_results(result)
 
 flow.run()
 
