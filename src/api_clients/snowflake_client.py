82b703666477b7b567f52330a71c9ae232d38b8f
Mathijs Miermans <m.miermans@gmail.com>
2022-01-13 16:44:17 -0800

    Fix error when secret is loaded during registration

--- src/api_clients/snowflake_client.py
+++ src/api_clients/snowflake_client.py
@@ -4,23 +4,25 @@ from prefect.tasks.snowflake import SnowflakeQuery
 from prefect.tasks.secrets import PrefectSecret
 from snowflake.connector import DictCursor
 
-private_key = PrefectSecret('SNOWFLAKE_PRIVATE_KEY').run().encode()
-snowflake_passphrase = PrefectSecret('SNOWFLAKE_PASSPHRASE').run().encode()
-
-p_key = serialization.load_pem_private_key(
-    private_key,
-    password=snowflake_passphrase,
-    backend=default_backend()
-)
-
-pkb = p_key.private_bytes(
-    encoding=serialization.Encoding.DER,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption())
 
 # https://docs.prefect.io/api/latest/tasks/aws.html#awssecretsmanager
 
 def get_query():
+    # PrefectSecret.run can only be called during Flow run, not during registration.
+    private_key = PrefectSecret('SNOWFLAKE_PRIVATE_KEY').run().encode()
+    snowflake_passphrase = PrefectSecret('SNOWFLAKE_PASSPHRASE').run().encode()
+
+    p_key = serialization.load_pem_private_key(
+        private_key,
+        password=snowflake_passphrase,
+        backend=default_backend()
+    )
+
+    pkb = p_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption())
+
     snowflake_account = PrefectSecret('SNOWFLAKE_ACCOUNT').run()
     snowflake_user = PrefectSecret('SNOWFLAKE_USER').run()
     return SnowflakeQuery(
