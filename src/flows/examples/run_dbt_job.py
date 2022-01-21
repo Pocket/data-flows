e7e14b44e93840593078af579a81da68a8e6863d
Gaurang Katre <gkatre@gmail.com>
2022-01-21 15:05:05 -0800

    task dependencies

--- src/flows/examples/run_dbt_job.py
+++ src/flows/examples/run_dbt_job.py
@@ -5,21 +5,26 @@ import prefect
 
 DBT_CLOUD_JOB_ID = 52822
 
-@task(trigger=all_successful)
-def log_task(message):
+@task
+def run_before_dbt_job():
     logger = prefect.context.get("logger")
-    logger.info(f'{message}')
+    logger.info(f'Starting Test DBT job')
 
 @task(trigger=all_successful)
 def run_dbt_job(cause: str, job_id: int) -> dict:
     return dbt.DbtCloudRunJob().run(cause=cause, job_id=job_id, wait_for_job_run_completion=True)
 
+@task(trigger=all_successful)
+def run_after_dbt_job():
+    logger = prefect.context.get("logger")
+    logger.info(f'Ending Test DBT job')
+
 with Flow("dbt_job_run") as flow:
-    start_flow_task = log_task('Starting Test DBT job')
+    before_dbt_job_task = run_before_dbt_job()
     dbt_run_result = run_dbt_job(cause='Test DBT Run', job_id=DBT_CLOUD_JOB_ID)
-    end_flow_task = log_task(f'Ending Test DBT job\n{dbt_run_result}')
-    dbt_run_result.set_upstream(start_flow_task)
-    dbt_run_result.set_downstream(end_flow_task)
+    after_dbt_job_task = run_after_dbt_job()
+    dbt_run_result.set_upstream(before_dbt_job_task)
+    dbt_run_result.set_downstream(after_dbt_job_task)
 
 # for execution in development only
 if __name__ == "__main__":
