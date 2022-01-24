df929ce5b69aae50b70a97f5192f97386900fc87
Chelsea Troy <chelsea.dommert@gmail.com>
2022-01-24 14:03:27 -0600

    base64 encode unencrypted private key

--- src/api_clients/snowflake_client.py
+++ src/api_clients/snowflake_client.py
@@ -1,7 +1,7 @@
+import base64
 import os
 from typing import Any
 
-from prefect.tasks.secrets import PrefectSecret
 from prefect.tasks.snowflake import SnowflakeQuery
 from prefect.utilities.tasks import defaults_from_attrs
 from snowflake.connector import DictCursor
@@ -12,7 +12,6 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
     def __init__(
             self,
             private_key_env_var_name: str = "SNOWFLAKE_PRIVATE_KEY",
-            private_key_passphrase_env_var_name: str = "SNOWFLAKE_PRIVATE_KEY_PASSPHRASE",
             account_env_var_name: str = "SNOWFLAKE_ACCOUNT",
             user_env_var_name: str = "SNOWFLAKE_USER",
             role_env_var_name: str = "SNOWFLAKE_ROLE",
@@ -21,7 +20,6 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
     ):
         super().__init__(**kwargs)
         self.private_key_env_var_name = private_key_env_var_name
-        self.private_key_passphrase_env_var_name = private_key_passphrase_env_var_name
         self.account_env_var_name = account_env_var_name
         self.user_env_var_name = user_env_var_name
         self.role_env_var_name = role_env_var_name
@@ -29,7 +27,6 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
 
     @defaults_from_attrs(
         "private_key_env_var_name",
-        "private_key_passphrase_env_var_name",
         "account_env_var_name",
         "user_env_var_name",
         "role_env_var_name",
@@ -39,7 +36,6 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
             self,
             private_key_env_var_name: str = None,
             private_key: bytes = None,
-            private_key_passphrase_env_var_name: str = None,
             account_env_var_name: str = None,
             account: str = None,
             user_env_var_name: str = None,
@@ -50,7 +46,7 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
             warehouse: str = None,
             **kwargs):
         if private_key is None and private_key_env_var_name in os.environ:
-            private_key = os.environ.get(private_key_env_var_name).encode()
+            private_key = base64.b64decode(os.environ.get(private_key_env_var_name))
 
         if account is None and account_env_var_name in os.environ:
             account = os.environ.get(account_env_var_name)
@@ -64,19 +60,6 @@ class AuthenticableSnowflakeQuery(SnowflakeQuery):
         if warehouse is None and warehouse_env_var_name in os.environ:
             warehouse = os.environ.get(warehouse_env_var_name)
 
-        if private_key_passphrase_env_var_name:
-            private_key_passphrase = os.environ[private_key_passphrase_env_var_name].encode()
-
-            private_key = serialization.load_pem_private_key(
-                private_key,
-                password=private_key_passphrase,
-                backend=default_backend()
-            )
-
-            private_key = private_key.private_bytes(
-                encoding=serialization.Encoding.DER,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption())
 
         super().run(
             private_key=private_key,
