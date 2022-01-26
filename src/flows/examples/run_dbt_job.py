6ae2c24d1d9f1b5b3e005de3c12a901767dc9ed9
Gaurang Katre <gkatre@gmail.com>
2022-01-26 10:11:33 -0800

    removing the "all_successful" default

--- src/flows/examples/run_dbt_job.py
+++ src/flows/examples/run_dbt_job.py
@@ -10,19 +10,19 @@ def run_before_dbt_job():
     logger = prefect.context.get("logger")
     logger.info(f'Starting Test DBT job')
 
-@task(trigger=all_successful)
-def run_dbt_job(cause: str, job_id: int) -> dict:
-    return dbt.DbtCloudRunJob().run(cause=cause, job_id=job_id, wait_for_job_run_completion=True)
-
-@task(trigger=all_successful)
+@task
 def run_after_dbt_job():
     logger = prefect.context.get("logger")
     logger.info(f'Ending Test DBT job')
 
 with Flow("dbt_job_run") as flow:
     before_dbt_job_task = run_before_dbt_job()
-    dbt_run_result = run_dbt_job(cause='Test DBT Run', job_id=DBT_CLOUD_JOB_ID)
+    dbt_run_result = dbt.DbtCloudRunJob()(cause='Test DBT Run', job_id=DBT_CLOUD_JOB_ID, wait_for_job_run_completion=True)
     after_dbt_job_task = run_after_dbt_job()
+
+    # Define dependencies between the above tasks explicitely with "set_upstream" and "set_downstream".
+    # Implicitily dependencies: If a return value of a prior task is used as an argument in a subsequent task,
+    # Prefect would automatically infer the task dependencies.
     dbt_run_result.set_upstream(before_dbt_job_task)
     dbt_run_result.set_downstream(after_dbt_job_task)
 
