71c7961c2e4bd395d6d75009869021bd3e60c374
Gaurang Katre <gkatre@gmail.com>
2022-02-02 14:39:20 -0800

    flow of flows

---
+++ src/flows/dbt_orchestration_flow.py
@@ -0,0 +1,27 @@
+from prefect import Flow
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from utils import config
+
+PREFECT_PROJECT_NAME = config.PREFECT_PROJECT_NAME
+FLOW_NAME = "DBT Orchestration Flow"
+DBT_FLOW_NAME = "DBT Job Flow"
+
+
+with Flow(FLOW_NAME) as flow:
+
+    dbt_job_flow_id = create_flow_run(flow_name=DBT_FLOW_NAME, project_name=PREFECT_PROJECT_NAME)
+    dbt_job_wait_task = wait_for_flow_run(dbt_job_flow_id, raise_final_state=True)
+
+    prereview_flow_id = create_flow_run(flow_name="PreReview Engagement to Feature Group Flow", project_name=PREFECT_PROJECT_NAME)
+    prereview_wait_task = wait_for_flow_run(prereview_flow_id, raise_final_state=True)
+
+    postreview_flow_id = create_flow_run(flow_name="PostReview Engagement to Feature Group Flow", project_name=PREFECT_PROJECT_NAME)
+    postreview_wait_task = wait_for_flow_run(postreview_flow_id, raise_final_state=True)
+
+    prereview_flow_id.set_upstream(dbt_job_wait_task)
+    postreview_flow_id.set_upstream(dbt_job_wait_task)
+
+# for execution in development only
+if __name__ == "__main__":
+    # flow.visualize()
+    flow.run()
--- src/utils/config.py
+++ src/utils/config.py
@@ -3,5 +3,7 @@ import os
 ENV_LOCAL = "local"
 ENV_DEV = "development"
 ENV_PROD = "production"
+PROJECT_LOCAL = "local"
 
 ENV = os.getenv("ENVIRONMENT", ENV_LOCAL)
+PREFECT_PROJECT_NAME = os.getenv('PREFECT_PROJECT_NAME', PROJECT_LOCAL)
