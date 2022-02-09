6532241d462fd6aaad418cb3521f64c5877c2357
Gaurang Katre <gkatre@gmail.com>
2022-02-09 14:56:17 -0800

    organizing environment variables

--- src/flows/examples/example_bq_to_gcs.py
+++ src/flows/examples/example_bq_to_gcs.py
@@ -1,24 +1,27 @@
 from prefect import Flow
 from prefect.tasks.gcp.bigquery import BigQueryTask
 
+from utils import config
+
 # GCS Bucket and Path to export BigQuery data
-gcs_bucket = 'pocket-airflow-nonprod-stage'
-gcs_path = 'gaurang/prefect_export_test/impression_stats_v1'
+gcs_bucket = config.GCS_BUCKET
+table_name = 'impression_stats_v1'
+gcs_path = f"{config.GCS_PATH}{table_name}"
 
 # BigQuery SQL to extract data
 #   date_partition : BigQuery table partition to extract (Note: the BQ table is partitioned by the
 #                   "submission_timestamp" field
-date_partition = '2022-02-08'
+date_partition = '20220207'
 extract_sql = f"""
         SELECT *
         FROM `moz-fx-data-shared-prod.activity_stream_live.impression_stats_v1`
-        where date(submission_timestamp) = @date_partition
+        where date(submission_timestamp) = PARSE_DATE("%Y%m%d", @date_partition)
     """
 
 # Export statement to export BQ data into GCS in compressed Parquet format
 export_sql = f"""
         EXPORT DATA OPTIONS(
-          uri='gs://{gcs_bucket}/{gcs_path}/{date_partition}/*.parq',
+          uri=@gcs_uri,
           format='PARQUET',
           compression='SNAPPY',
           overwrite=true) AS
@@ -29,7 +32,8 @@ with Flow("example Snowflake query") as flow:
     # BigQueryTask: (https://docs.prefect.io/api/latest/tasks/gcp.html#bigquerytask)
     bq_result = BigQueryTask()(
         query=export_sql,
-        query_params=[('date_partition', 'STRING', date_partition),],
+        query_params=[('date_partition', 'STRING', date_partition),
+                      ('gcs_uri', 'STRING', f'gs://{gcs_bucket}/{gcs_path}/{date_partition}/*.parq')],
     )
 
 if __name__ == "__main__":
