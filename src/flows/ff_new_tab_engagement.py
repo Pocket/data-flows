493b87ea04c27c1f0af5881f94071292112824b7
Gaurang Katre <gkatre@gmail.com>
2022-02-17 11:06:43 -0800

    wip

--- src/flows/ff_new_tab_engagement.py
+++ src/flows/ff_new_tab_engagement.py
@@ -31,6 +31,76 @@ export_sql = """
           and submission_timestamp > @last_executed_timestamp
     """
 
+import_sql = """
+        copy into development.gaurang_data_engineering.impression_stats_v1(
+              submission_timestamp,
+              additional_properties,
+              addon_version,
+              block,
+              click,
+              document_id,
+              impression_id,
+              loaded,
+              locale,
+              metadata,
+              normalized_app_name,
+              normalized_channel,
+              normalized_country_code,
+              normalized_os,
+              normalized_os_version,
+              page,
+              pocket,
+              profile_creation_date,
+              region,
+              release_channel,
+              sample_id,
+              shield_id,
+              source,
+              tiles,
+              user_prefs,
+              version,
+              experiments,
+              s3_filename_splitpath, 
+              s3_file_row_number
+            )
+        from (
+            select
+                to_timestamp($1:submission_timestamp::integer/1000000) as submission_timestamp,
+                $1:additional_properties::string as additional_properties,
+                $1:addon_version::string as addon_version,
+                $1:block::integer as block,
+                $1:click::integer as click,
+                $1:document_id::string as document_id,
+                $1:impression_id::string as impression_id,
+                $1:loaded::integer as loaded,
+                $1:locale::string as locale,
+                $1:metadata as metadata,
+                $1:normalized_app_name::string as normalized_app_name,
+                $1:normalized_channel::string as normalized_channel,
+                $1:normalized_country_code::string as normalized_country_code,
+                $1:normalized_os::string as normalized_os,
+                $1:normalized_os_version::string as normalized_os_version,
+                $1:page::string as page,
+                $1:pocket::integer as pocket,
+                $1:profile_creation_date::integer as profile_creation_date,
+                $1:region::string as region,
+                $1:release_channel::string as release_channel,
+                $1:sample_id::integer as sample_id,
+                $1:shield_id::string as shield_id,
+                $1:source::string as source,
+                $1:tiles as tiles,
+                $1:user_prefs::integer as user_prefs,
+                $1:version::string as version,
+                $1:experiments as experiments,
+                split(metadata$filename,'/'),
+                metadata$file_row_number
+            from @development.public.prefect_gcs_stage_parq/gaurang/prefect_export_test/impression_stats_v1/20220214/
+        )
+        file_format = (type = 'PARQUET')
+        on_error=skip_file
+    """
+
+
 @task
 def prepare_bq_export_statement(last_executed_timestamp: datetime):
     """
@@ -45,11 +115,12 @@ def prepare_bq_export_statement(last_executed_timestamp: datetime):
     """
     date_partition = last_executed_timestamp.strftime('%Y-%m-%d')
     gcs_date_partition_path = last_executed_timestamp.strftime('%Y%m%d')
+    batch_id = last_executed_timestamp.timestamp()
 
     gcs_bucket = config.GCS_BUCKET
     table_name = 'impression_stats_v1'
     gcs_path = f"{config.GCS_PATH}{table_name}"
-    gcs_uri = f'gs://{gcs_bucket}/{gcs_path}/{gcs_date_partition_path}/*.parq'
+    gcs_uri = f'gs://{gcs_bucket}/{gcs_path}/{gcs_date_partition_path}/{batch_id}_*.parq'
 
     logger = prefect.context.get("logger")
     logger.info(f"last_executed_timestamp: {str(last_executed_timestamp)}")
--- src/utils/config.py
+++ src/utils/config.py
@@ -9,5 +9,5 @@ GCS_PATH_DEFAULT = "/"
 ENVIRONMENT = os.getenv("ENVIRONMENT", ENV_LOCAL)
 PREFECT_PROJECT_NAME = os.getenv('PREFECT_PROJECT_NAME', PROJECT_LOCAL)
 
-GCS_BUCKET = 'pocket-airflow-prod-stage' if ENVIRONMENT == ENV_PROD else 'pocket-airflow-nonprod-stage'
+GCS_BUCKET = 'pocket-prefect-stage-prod' if ENVIRONMENT == ENV_PROD else 'pocket-prefect-stage-dev'
 GCS_PATH = os.getenv('GCS_PATH', GCS_PATH_DEFAULT)
