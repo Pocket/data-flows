63785eeb3e82591c8cdd5cff584eaa8667762729
Gaurang Katre <gkatre@users.noreply.github.com>
2022-02-24 08:52:05 -0800

    Newtab gcs to snowflake [FFRECSV2-147] (#38)
    * wip
    
    * wip
    
    * Passing multiple params to Snowflake import query
    
    * Snowflake Dev and Prod schema
    
    * removing some junk
    
    * prepare_exp_imp_params return value description
    
    * Updates based on suggestions from Jesh
    
    * update
    
    * setting GCS_PATH default

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,10 +4,16 @@ ENV_LOCAL = "local"
 ENV_DEV = "development"
 ENV_PROD = "production"
 PROJECT_LOCAL = "local"
-GCS_PATH_DEFAULT = "/"
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", ENV_LOCAL)
 PREFECT_PROJECT_NAME = os.getenv('PREFECT_PROJECT_NAME', PROJECT_LOCAL)
 
-GCS_BUCKET = 'pocket-airflow-prod-stage' if ENVIRONMENT == ENV_PROD else 'pocket-airflow-nonprod-stage'
+GCS_BUCKET = 'pocket-prefect-stage-prod' if ENVIRONMENT == ENV_PROD else 'pocket-prefect-stage-dev'
+GCS_PATH_DEFAULT = ENVIRONMENT
 GCS_PATH = os.getenv('GCS_PATH', GCS_PATH_DEFAULT)
+
+SNOWFLAKE_DEV_SCHEMA_DEFAULT = 'DEV_DATA_ENGINEERING'
+SNOWFLAKE_DEV_SCHEMA = os.getenv('SNOWFLAKE_DEV_SCHEMA', SNOWFLAKE_DEV_SCHEMA_DEFAULT)
+SNOWFLAKE_STAGE = 'prefect.public.prefect_gcs_stage_parq_prod' if ENVIRONMENT == ENV_PROD else 'development.public.prefect_gcs_stage_parq_dev'
+SNOWFLAKE_DB = 'prefect' if ENVIRONMENT == ENV_PROD else 'development'
+SNOWFLAKE_MOZILLA_SCHEMA = 'mozilla' if ENVIRONMENT == ENV_PROD else SNOWFLAKE_DEV_SCHEMA
