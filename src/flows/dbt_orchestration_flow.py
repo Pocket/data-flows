4d7e0e036710240684981ca47494c43418ed6d0f
Mathijs Miermans <m.miermans@gmail.com>
2022-03-28 20:52:09 -0700

    Revert "Set custom names for flow-of-flow"
    This reverts commit 23911bea8adfa7eb6c229ab027faded4fd74efee.

--- src/flows/dbt_orchestration_flow.py
+++ src/flows/dbt_orchestration_flow.py
@@ -5,7 +5,6 @@ from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefect.executors import LocalDaskExecutor
 from prefect.schedules import IntervalSchedule
 from utils import config
-from utils.config import PREFECT_PROJECT_NAME
 
 # DBT Job Flow
 from flows import dbt_job_flow
@@ -26,51 +25,19 @@ else:
 
 with Flow(FLOW_NAME, schedule=schedule, executor=LocalDaskExecutor()) as flow:
 
-    dbt_job_flow_id = create_flow_run(
-        flow_name=dbt_job_flow.FLOW_NAME,
-        project_name=PREFECT_PROJECT_NAME,
-        task_run_name=f"create {dbt_job_flow.FLOW_NAME}",
-    )
-    dbt_job_wait_task = wait_for_flow_run(
-        dbt_job_flow_id,
-        raise_final_state=True,
-        task_run_name=f"wait for {dbt_job_flow.FLOW_NAME}"
-    )
+    dbt_job_flow_id = create_flow_run(flow_name=dbt_job_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
+    dbt_job_wait_task = wait_for_flow_run(dbt_job_flow_id, raise_final_state=True)
 
-    braze_flow_id = create_flow_run(
-        flow_name=braze_update_flow.FLOW_NAME,
-        project_name=config.PREFECT_PROJECT_NAME,
-        task_run_name=f"create {braze_update_flow.FLOW_NAME}",
-    )
-    braze_wait_task = wait_for_flow_run(
-        braze_flow_id,
-        raise_final_state=True,
-        task_run_name=f"wait for {braze_update_flow.FLOW_NAME}",
-    )
+    braze_flow_id = create_flow_run(flow_name=braze_update_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
+    braze_wait_task = wait_for_flow_run(braze_flow_id, raise_final_state=True)
     braze_flow_id.set_upstream(dbt_job_wait_task)
 
-    prereview_flow_id = create_flow_run(
-        flow_name=prereview_engagement_feature_store_flow.FLOW_NAME,
-        project_name=config.PREFECT_PROJECT_NAME,
-        task_run_name=f"create {prereview_engagement_feature_store_flow.FLOW_NAME}",
-    )
-    prereview_wait_task = wait_for_flow_run(
-        prereview_flow_id,
-        raise_final_state=True,
-        task_run_name=f"wait for {prereview_engagement_feature_store_flow.FLOW_NAME}",
-    )
+    prereview_flow_id = create_flow_run(flow_name=prereview_engagement_feature_store_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
+    prereview_wait_task = wait_for_flow_run(prereview_flow_id, raise_final_state=True)
     prereview_flow_id.set_upstream(dbt_job_wait_task)
 
-    postreview_flow_id = create_flow_run(
-        flow_name=postreview_engagement_feature_store_flow.FLOW_NAME,
-        project_name=config.PREFECT_PROJECT_NAME,
-        task_run_name=f"create {postreview_engagement_feature_store_flow.FLOW_NAME}",
-    )
-    postreview_wait_task = wait_for_flow_run(
-        postreview_flow_id,
-        raise_final_state=True,
-        task_run_name=f"wait for {postreview_engagement_feature_store_flow.FLOW_NAME}",
-    )
+    postreview_flow_id = create_flow_run(flow_name=postreview_engagement_feature_store_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
+    postreview_wait_task = wait_for_flow_run(postreview_flow_id, raise_final_state=True)
     postreview_flow_id.set_upstream(dbt_job_wait_task)
 
 # for execution in development only
