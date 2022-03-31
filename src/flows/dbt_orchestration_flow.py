47156a8d4c13727ad0ae9eeeb81a6e09c4837638
Mathijs Miermans <m.miermans@gmail.com>
2022-03-31 07:54:52 -0700

    Name the downstream Dbt tasks and use a for-loop

--- src/flows/dbt_orchestration_flow.py
+++ src/flows/dbt_orchestration_flow.py
@@ -15,6 +15,12 @@ from flows import postreview_engagement_feature_store_flow
 FLOW_NAME = "DBT Orchestration Flow"
 DBT_CLOUD_JOB_ID = 52822
 
+# List of flow names that will be run after the Dbt job run has finished successfully.
+DBT_DOWNSTREAM_FLOW_NAMES = [
+    # braze_update_flow.FLOW_NAME,  # Enable this and set production/braze_update_flow/last_loaded_at on April 13th.
+    prereview_engagement_feature_store_flow.FLOW_NAME,
+    postreview_engagement_feature_store_flow.FLOW_NAME,
+]
 
 # Schedule to run every 5 minutes
 if config.ENVIRONMENT == config.ENV_PROD:
@@ -26,21 +32,20 @@ with Flow(FLOW_NAME, schedule=schedule, executor=LocalDaskExecutor()) as flow:
 
     dbt_run_result = dbt.DbtCloudRunJob()(cause=FLOW_NAME, job_id=DBT_CLOUD_JOB_ID, wait_for_job_run_completion=True)
 
-    # TODO: To start updating Braze based on Snowplow:
-    # 1. In the KV-store set production/braze_update_flow/last_loaded_at to the etl_tstamp of the first Snowplow event
-    #    we should process.
-    # 2. Enable the code below and merge it into main.
-    # braze_flow_id = create_flow_run(flow_name=braze_update_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
-    # braze_wait_task = wait_for_flow_run(braze_flow_id, raise_final_state=True)
-    # braze_flow_id.set_upstream(dbt_run_result)
+    # Create and wait for all flows that should be run downstream of the above Dbt job run.
+    for downstream_flow_name in DBT_DOWNSTREAM_FLOW_NAMES:
+        flow_id = create_flow_run(
+            flow_name=downstream_flow_name,
+            project_name=config.PREFECT_PROJECT_NAME,
+            task_args=dict(name=f"create_flow_run({downstream_flow_name})"),
+            upstream_tasks=[dbt_run_result],
+        )
 
-    prereview_flow_id = create_flow_run(flow_name=prereview_engagement_feature_store_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
-    prereview_wait_task = wait_for_flow_run(prereview_flow_id, raise_final_state=True)
-    prereview_flow_id.set_upstream(dbt_run_result)
-
-    postreview_flow_id = create_flow_run(flow_name=postreview_engagement_feature_store_flow.FLOW_NAME, project_name=config.PREFECT_PROJECT_NAME)
-    postreview_wait_task = wait_for_flow_run(postreview_flow_id, raise_final_state=True)
-    postreview_flow_id.set_upstream(dbt_run_result)
+        wait_for_flow_run(
+            flow_id,
+            raise_final_state=True,
+            task_args=dict(name=f"create_flow_run({downstream_flow_name})"),
+        )
 
 # for execution in development only
 if __name__ == "__main__":
