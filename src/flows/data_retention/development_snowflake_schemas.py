abcfa48408699ed38d4cc2b21d9889fad4d3c354
Jeshua Borges <jesh@jesh.me>
2022-05-26 09:16:36 -0700

    Delete old Snowflake DEVELOPMENT db schemas
    Delete any schema greater than 89 days old nightly.

---
+++ src/flows/data_retention/development_snowflake_schemas.py
@@ -0,0 +1,38 @@
+"""DELETE all DEVELOPMENT DB schemas older than 89 days
+
+Reference: https://getpocket.atlassian.net/wiki/spaces/CP/pages/2703949851/Snowflake+Data+Deletion+and+Retention
+"""
+from prefect import Flow, task, unmapped
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import CronClock
+
+from api_clients.pocket_snowflake_query import PocketSnowflakeQuery
+from utils.flow import get_flow_name
+
+FLOW_NAME = get_flow_name(__file__)
+DATABASE = "DEVELOPMENT"
+
+# Run daily at midnight UTC.
+SCHEDULE = Schedule(clocks=[CronClock("0 0 * * *")])
+
+GET_SCHEMAS_SQL = """
+SELECT schema_name
+FROM information_schema.schemata
+WHERE created < DATEADD("day", -89, CURRENT_TIMESTAMP());
+"""
+
+DROP_SCHEMA_SQL = """
+DROP SCHEMA {schema_name}
+"""
+
+
+@task()
+def delete_schema(row: tuple[str], executor: PocketSnowflakeQuery):
+    return executor(query=DROP_SCHEMA_SQL, data={"schema_name": row[0]})
+
+
+with Flow(FLOW_NAME) as flow:
+    executor = PocketSnowflakeQuery(database=DATABASE)
+    results = executor(query=GET_SCHEMAS_SQL)
+    delete_schema.map(results, unmapped(executor))
+
