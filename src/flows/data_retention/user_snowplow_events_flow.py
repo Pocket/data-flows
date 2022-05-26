def8f5af7575d506f456561881c57661e7f5ccdf
Jeshua Borges <jesh@jesh.me>
2022-05-26 12:03:00 -0700

    Orchestrate DBT model deletion after event cleanup

--- src/flows/data_retention/user_snowplow_events_flow.py
+++ src/flows/data_retention/user_snowplow_events_flow.py
@@ -6,6 +6,7 @@
 from prefect import Flow
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import CronClock
+from prefect.tasks.dbt import DbtCloudRunJob
 
 from api_clients.pocket_snowflake_query import PocketSnowflakeQuery
 from utils.flow import get_flow_name
@@ -15,14 +16,16 @@ FLOW_NAME = get_flow_name(__file__)
 # Run first day of month at midnight UTC.
 SCHEDULE = Schedule(clocks=[CronClock("0 0 1 * *")])
 
+DBT_MODEL_CLEANUP_JOB_ID = 17774
+
 DELETE_SQL = """
 DELETE FROM "SNOWPLOW"."ATOMIC"."EVENTS" as e
 USING "ANALYTICS"."DBT_STAGING"."STG_ACCOUNT_DELETIONS" as d 
 WHERE e.CONTEXTS_COM_POCKET_USER_1[0]:hashed_user_id = d.hashed_user_id
-AND d.happened_at > DATEADD("day", -90, CURRENT_TIMESTAMP())
 """
 
 with Flow(FLOW_NAME, schedule=SCHEDULE) as flow:
-    execute = PocketSnowflakeQuery()
-    extract_result = execute(query=DELETE_SQL)
+    snowflake = PocketSnowflakeQuery()
+    delete_result = snowflake(query=DELETE_SQL)
+    DbtCloudRunJob()(cause=FLOW_NAME, job_id=DBT_MODEL_CLEANUP_JOB_ID, wait_for_job_run_completion=True, upstream_tasks=[delete_result])
 
