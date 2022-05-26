237bf36381784064737f2ad1e185b349c0700046
Jeshua Borges <jesh@jesh.me>
2022-05-26 16:56:21 -0700

    Use unique snowflake credentials for data retention

--- src/flows/data_retention/user_snowplow_events_flow.py
+++ src/flows/data_retention/user_snowplow_events_flow.py
@@ -3,12 +3,13 @@
  Reference: https://getpocket.atlassian.net/wiki/spaces/CP/pages/2703949851/Snowflake+Data+Deletion+and+Retention
 """
 
-from prefect import Flow
+from prefect import Flow, task
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import CronClock
 from prefect.tasks.dbt import DbtCloudRunJob
+from prefect.tasks.snowflake import SnowflakeQuery
 
-from api_clients.pocket_snowflake_query import PocketSnowflakeQuery
+from utils.config import SNOWFLAKE_DATA_RETENTION_CONNECTION_DICT
 from utils.flow import get_flow_name
 
 FLOW_NAME = get_flow_name(__file__)
@@ -24,8 +25,17 @@ USING "ANALYTICS"."DBT_STAGING"."STG_ACCOUNT_DELETIONS" as d
 WHERE e.CONTEXTS_COM_POCKET_USER_1[0]:hashed_user_id = d.hashed_user_id
 """
 
-with Flow(FLOW_NAME, schedule=SCHEDULE) as flow:
-    snowflake = PocketSnowflakeQuery()
-    delete_result = snowflake(query=DELETE_SQL)
-    DbtCloudRunJob()(cause=FLOW_NAME, job_id=DBT_MODEL_CLEANUP_JOB_ID, wait_for_job_run_completion=True, upstream_tasks=[delete_result])
 
+@task()
+def delete_user_snowplow_events():
+    return SnowflakeQuery(**SNOWFLAKE_DATA_RETENTION_CONNECTION_DICT)(query=DELETE_SQL)
+
+
+@task()
+def delete_user_dbt_rows():
+    DbtCloudRunJob()(cause=FLOW_NAME, job_id=DBT_MODEL_CLEANUP_JOB_ID, wait_for_job_run_completion=True)
+
+
+with Flow(FLOW_NAME, schedule=SCHEDULE) as flow:
+    delete_result = delete_user_snowplow_events()
+    delete_user_dbt_rows(upstream_tasks=[delete_result])
