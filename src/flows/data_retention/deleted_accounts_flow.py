29caa44bbbb8d251f44d9f64af7b32b98f32f370
Gaurang Katre <gkatre@gmail.com>
2022-06-22 16:55:32 -0700

    Updates to the flow and parameters for raw data DB/Schema

--- src/flows/data_retention/deleted_accounts_flow.py
+++ src/flows/data_retention/deleted_accounts_flow.py
@@ -12,7 +12,7 @@ def query_file(file_name: str, **kwargs):
     cfg = config.SNOWFLAKE_DATA_RETENTION_CONNECTION_DICT
     # SnowflakeQueriesFromFile has a bug where it throws a validation error if user and account
     # are not passed to `run()`.
-    return SnowflakeQueriesFromFile(**cfg)(
+    return SnowflakeQueriesFromFile(**cfg).run(
         file_path=get_flow_file_path(__file__, file_name),
         account=cfg['account'],
         user=cfg['user'],
@@ -22,13 +22,26 @@ def query_file(file_name: str, **kwargs):
 
 # with Flow(FLOW_NAME, schedule=get_cron_schedule(cron="0 0 1 * *")) as flow:
 with Flow(FLOW_NAME) as flow:
-    backup_results = [
-        query_file('deleted_account_emails.sql'),
-        query_file('deleted_account_users.sql'),
-    ]
+    # Maintain a list of deleted accounts in a protected DB/Schema tables
+    add_deleted_users_result = query_file(file_name='deleted_account_users.sql')
+    add_deleted_emails_result = query_file(file_name='deleted_account_emails.sql')
+    add_deleted_emails_result.set_upstream(add_deleted_users_result)
 
-    query_file('delete_snowplow_events.sql', upstream_tasks=backup_results)
-    query_file('delete_raw_user_rows.sql', upstream_tasks=backup_results)
+    backup_results = [
+        add_deleted_users_result,
+        add_deleted_emails_result,
+    ]
+    # Delete Snowplow Raw events of deleted user accounts
+    delete_snowplow_events_result = query_file(file_name='delete_snowplow_events.sql',
+                                               database=config.SNOWFLAKE_SNOWPLOW_DB,
+                                               schema=config.SNOWFLAKE_SNOWPLOW_SCHEMA,
+                                               upstream_tasks=backup_results)
+
+    # Delete Raw data of deleted user accounts from other streaming sources
+    delete_raw_data_result = query_file(file_name='delete_raw_user_rows.sql',
+                                        database=config.SNOWFLAKE_RAWDATA_DB,
+                                        schema=config.SNOWFLAKE_RAWDATA_FIREHOSE_SCHEMA,
+                                        upstream_tasks=backup_results)
 
 if __name__ == "__main__":
     flow.run()
