340259711e69cbcf8887bba61c521f59377765e5
Gaurang Katre <gkatre@gmail.com>
2022-08-02 15:36:50 -0700

    Topic Flow to generate Corpus Topic Candidate item sets

--- src/flows/recommendation_api/topics_flow.py
+++ src/flows/recommendation_api/topics_flow.py
@@ -1,5 +1,5 @@
 from prefect import Flow, Parameter, unmapped, task
-
+from prefect.executors import LocalDaskExecutor
 from api_clients.pocket_snowflake_query import PocketSnowflakeQuery, OutputType
 from common_tasks.corpus_candidate_set import (
     create_corpus_candidate_set_record,
@@ -12,47 +12,48 @@ from utils.flow import get_flow_name, get_interval_schedule
 
 FLOW_NAME = get_flow_name(__file__)
 
-CORPUS_CANDIDATE_SET_ID = 'a765b8ab-9631-4046-9d2e-8706324efc96'
-
 # Export approved corpus items by language and recency
 EXPORT_CORPUS_ITEMS_SQL = """
-SELECT *
+SELECT 
+    approved_corpus_item_external_id as ID, 
+    topic
 FROM "ANALYTICS"."DBT"."APPROVED_CORPUS_ITEMS"
-WHERE APPROVED_AT >= DATE_ADD('day', -90, CURRENT_DATE())
-AND TOPIC = %(topic)s
-AND LANGUAGE = 'en'
+WHERE REVIEWED_CORPUS_ITEM_UPDATED_AT >= DATEADD('day', -90, current_timestamp())
+AND TOPIC = %(CORPUS_TOPIC_ID)s
+AND LANGUAGE = 'EN'
 """
 
 GET_TOPICS_SQL = """
-SELECT id, topic 
-FROM "ANALYTICS"."DBT"."CORPUS_CANDIDATE_SETS"
+SELECT 
+    curated_corpus_candidate_set_id, 
+    corpus_topic_id
+FROM analytics.dbt.static_corpus_candidate_set_topics
 """
 
 @task()
-def get_topic_ids(topics):
-    return [i['corpus_candidate_set_id'] for i in topics]
+def get_candidate_set_ids(topics):
+    return [i['CURATED_CORPUS_CANDIDATE_SET_ID'] for i in topics]
 
-
-with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30)) as flow:
+with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30), executor=LocalDaskExecutor()) as flow:
     query = PocketSnowflakeQuery(
         database=config.SNOWFLAKE_ANALYTICS_DATABASE,
         schema=config.SNOWFLAKE_ANALYTICS_DBT_SCHEMA,
+        output_type = OutputType.DICT
     )
 
-    topics = query(query=GET_TOPICS_SQL, output_type=OutputType.DICT)
-    topic_ids = get_topic_ids(topics)
+    topics = query(query=GET_TOPICS_SQL)
+
+    candidate_set_ids = get_candidate_set_ids(topics)
 
     topic_corpus_items = query.map(data=topics, query=unmapped(EXPORT_CORPUS_ITEMS_SQL))
 
     topic_corpus_items = validate_corpus_items.map(topic_corpus_items)
 
     feature_group_record = create_corpus_candidate_set_record.map(
-        id=topic_ids,
+        id=candidate_set_ids,
         corpus_items=topic_corpus_items,
     )
-    load_feature_record(feature_group_record, feature_group_name=feature_group)
+    load_feature_record.map(feature_group_record, feature_group_name=unmapped(feature_group))
 
 if __name__ == "__main__":
     flow.run()
-
-
