eeaa8ed0975a8e547ddf0b76e4b40a2201bf6a0a
Matthew Cooper <mcooper@getpocket.com>
2022-08-01 18:24:12 -0700

    adding candidate set for pocket hits

--- src/flows/recommendation_api/pockethits_flow.py
+++ src/flows/recommendation_api/pockethits_flow.py
@@ -14,38 +14,35 @@ from utils.flow import get_flow_name, get_interval_schedule
 from api_clients.sqs import put_results, RecommendationCandidate, NewTabFeedID
 
 '''
-Builds Coronovirus candidate set and writes it to SQS and Feature Store
+Builds pocket hits candidate sets for en and de and writes it to SQS and Feature Store
 '''
-
-# todo: switch to generic curated topic flow
-
 
 FLOW_NAME = get_flow_name(__file__)
 
-CORONAVIRUS_CANDIDATE_SET_ID = 'c29fbf48-0093-49d2-ae55-4e01a3cf800e'
+POCKETHITS_EN_CANDIDATE_SET_ID = "92411893-ebdb-4a43-ad29-aa79e56e2136"
 
-CORONAVIRUS_SQL = """
+POCKETHITS_SQL = """
 SELECT
     s.resolved_id as id,   
     c.top_domain_name as publisher,
-    s.resolved_url,
+    s.title,
     s.scheduled_corpus_item_scheduled_at,
     s.scheduled_surface_name,
+    s.scheduled_surface_id,
     s.topic,
     s.approved_corpus_item_external_id
 FROM scheduled_corpus_items as s
 JOIN content as c
   ON c.content_id = s.content_id
-WHERE s.TOPIC = 'CORONAVIRUS'
-  AND s.scheduled_surface_id = 'NEW_TAB_EN_US'
-  AND s.scheduled_corpus_item_scheduled_at  BETWEEN (current_date - 7) AND current_date  
+WHERE s.scheduled_surface_id = %(SURFACE_GUID)s
+  AND s.scheduled_corpus_item_scheduled_at  BETWEEN (current_date - %(MAX_AGE_DAYS)s) AND current_date 
 ORDER BY s.scheduled_corpus_item_scheduled_at DESC
-LIMIT 60
 """
 
 
 @task()
 def transform_to_candidates(records: dict) -> List[RecommendationCandidate]:
+    # recs api candidate sets don't yet include topic information
     return [RecommendationCandidate(
         item_id=rec["ID"],
         publisher=rec["PUBLISHER"],
@@ -55,17 +52,19 @@ def transform_to_candidates(records: dict) -> List[RecommendationCandidate]:
 
 @task()
 def transform_to_corpus_items(records: dict) -> List[dict]:
+    # corpus candidate sets don't yet include publisher information
     return [
         {'ID': rec['ID'], 'TOPIC': rec['TOPIC']}
         for rec in records]
 
 
 with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30)) as flow:
+
     records = PocketSnowflakeQuery()(
-        query=CORONAVIRUS_SQL,
+        query=POCKETHITS_SQL,
         data={
-            'scheduled_at_start_day': -60,
-            'language': 'EN',
+            "MAX_AGE_DAYS": 9,
+            "SURFACE_GUID": "POCKET_HITS_EN_US"
         },
         database=config.SNOWFLAKE_ANALYTICS_DATABASE,
         schema=config.SNOWFLAKE_ANALYTICS_DBT_SCHEMA,
@@ -76,14 +75,14 @@ with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30)) as flow:
     corpus_items = transform_to_corpus_items(records)
     corpus_items = validate_corpus_items(corpus_items)
     feature_group_record = create_corpus_candidate_set_record(
-        id=CORONAVIRUS_CANDIDATE_SET_ID,
-        corpus_items=corpus_items,
+        id=POCKETHITS_EN_CANDIDATE_SET_ID,
+        corpus_items=corpus_items
     )
     load_feature_record(feature_group_record, feature_group_name=feature_group)
 
     # write to sqs
     candidates = transform_to_candidates(records)
-    put_results(CORONAVIRUS_CANDIDATE_SET_ID, candidates, curated=True)
+    put_results(POCKETHITS_EN_CANDIDATE_SET_ID, candidates, curated=True)
 
 if __name__ == "__main__":
     flow.run()
