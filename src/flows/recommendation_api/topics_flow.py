31971f35ff2a0f419d593ffd37c7311b27dcb06b
Gaurang Katre <gkatre@users.noreply.github.com>
2022-08-03 12:17:39 -0700

    Update explicit capitalized naming and unique task return values
    Updates:
    - explicit capitalized naming of columns in SQLs
    - Making task parameters and return values unique for accurately define flow path

--- src/flows/recommendation_api/topics_flow.py
+++ src/flows/recommendation_api/topics_flow.py
@@ -15,8 +15,8 @@ FLOW_NAME = get_flow_name(__file__)
 # Export approved corpus items by language and recency
 EXPORT_CORPUS_ITEMS_SQL = """
 SELECT 
-    approved_corpus_item_external_id as ID, 
-    topic
+    approved_corpus_item_external_id as "ID", 
+    topic as "TOPIC"
 FROM "ANALYTICS"."DBT"."APPROVED_CORPUS_ITEMS"
 WHERE REVIEWED_CORPUS_ITEM_UPDATED_AT >= DATEADD('day', -90, current_timestamp())
 AND TOPIC = %(CORPUS_TOPIC_ID)s
@@ -25,8 +25,8 @@ AND LANGUAGE = 'EN'
 
 GET_TOPICS_SQL = """
 SELECT 
-    curated_corpus_candidate_set_id, 
-    corpus_topic_id
+    curated_corpus_candidate_set_id as "CURATED_CORPUS_CANDIDATE_SET_ID", 
+    corpus_topic_id as "CORPUS_TOPIC_ID"
 FROM analytics.dbt.static_corpus_candidate_set_topics
 """
 
@@ -47,11 +47,11 @@ with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30), executor=LocalD
 
     topic_corpus_items = query.map(data=topics, query=unmapped(EXPORT_CORPUS_ITEMS_SQL))
 
-    topic_corpus_items = validate_corpus_items.map(topic_corpus_items)
+    valid_topic_corpus_items = validate_corpus_items.map(topic_corpus_items)
 
     feature_group_record = create_corpus_candidate_set_record.map(
         id=candidate_set_ids,
-        corpus_items=topic_corpus_items,
+        corpus_items=valid_topic_corpus_items,
     )
     load_feature_record.map(feature_group_record, feature_group_name=unmapped(feature_group))
 
