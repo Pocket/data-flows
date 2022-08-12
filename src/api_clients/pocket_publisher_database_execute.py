689b4cc2731c3a7397c9ca317eb65e0b5f9244fc
Daniel Brooks <bass_rock@me.com>
2022-08-12 11:28:10 -0700

    feat(publisher): move the publisher data job to prefect

---
+++ src/api_clients/pocket_publisher_database_execute.py
@@ -0,0 +1,97 @@
+import os
+from typing import Any
+
+from prefect.tasks.mysql import MySQLExecute
+from prefect.utilities.tasks import defaults_from_attrs
+
+
+class PocketPublisherDatabaseExecute(MySQLExecute):
+    """
+    PocketPublisherDatabaseQuery automatically connects to our Publisher MySQL Database if the right environment variables are set:
+    - POCKET_PUBLISHER_DATABASE_HOST
+    - POCKET_PUBLISHER_DATABASE_PORT
+    - POCKET_PUBLISHER_DATABASE_DBNAME
+    - POCKET_PUBLISHER_DATABASE_USER
+    - POCKET_PUBLISHER_DATABASE_PASSWORD
+    """
+
+    def __init__(
+            self,
+            query: str = None,
+            database_host_env_var_name: str = "POCKET_PUBLISHER_DATABASE_HOST",
+            database_port_env_var_name: str = "POCKET_PUBLISHER_DATABASE_PORT",
+            database_dbname_env_var_name: str = "POCKET_PUBLISHER_DATABASE_DBNAME",
+            database_user_var_name: str = "POCKET_PUBLISHER_DATABASE_USER",
+            database_password_var_name: str = "POCKET_PUBLISHER_DATABASE_PASSWORD",
+            cursor_type: str = 'cursor',
+            **kwargs: Any,  # See the base SnowflakeQuery class for additional arguments
+    ):
+        super().__init__(
+            query=query,
+            cursor_type=cursor_type,
+            **kwargs
+        )
+        self.database_dbname_env_var_name = database_dbname_env_var_name
+        self.database_user_var_name = database_user_var_name
+        self.database_host_env_var_name = database_host_env_var_name
+        self.database_port_env_var_name = database_port_env_var_name
+        self.database_password_var_name = database_password_var_name
+
+    @defaults_from_attrs(
+        "query",
+        "database_dbname_env_var_name",
+        "database_user_var_name",
+        "database_host_env_var_name",
+        "database_port_env_var_name",
+        "database_password_var_name",
+        "cursor_type",
+    )
+    def run(
+            self,
+            query: str = None,
+            database_dbname_env_var_name: str = None,
+            database_dbname: str = None,
+            database_user_var_name: str = None,
+            database_user: str = None,
+            database_host_env_var_name: str = None,
+            database_host: str = None,
+            database_port_env_var_name: str = None,
+            database_port: int = None,
+            database_password_var_name: str = None,
+            database_password: str = None,
+            cursor_type: str = 'cursor',
+            **kwargs):
+
+        if database_dbname is None and database_dbname_env_var_name in os.environ:
+            database_dbname = os.environ.get(database_dbname_env_var_name)
+
+        if database_user is None and database_user_var_name in os.environ:
+            database_user = os.environ.get(database_user_var_name)
+
+        if database_host is None and database_host_env_var_name in os.environ:
+            database_host = os.environ.get(database_host_env_var_name)
+
+        if database_port is None and database_port_env_var_name in os.environ:
+            database_port = int(os.environ.get(database_port_env_var_name))
+
+        if database_password is None and database_password_var_name in os.environ:
+            database_password = os.environ.get(database_password_var_name)
+
+        self.logger.info(f"Connecting using database={kwargs.get('database')} and schema={kwargs.get('schema')}."
+                         f" (If 'None' then the default database/schema is used.)")
+        self.logger.info(query)
+
+        query_result = super().run(
+            query=query,
+            host=database_host,
+            port=database_port,
+            user=database_user,
+            password=database_password,
+            db_name=database_dbname,
+            cursor_type=cursor_type,
+            **kwargs
+        )
+
+        self.logger.info(f'Rows Updated: {query_result}')
+
+        return query_result
---
+++ src/flows/publisher_data/update_pocket_score_data_flow.py
@@ -0,0 +1,196 @@
+from prefect import Flow, task
+
+from utils import config
+from utils.flow import get_flow_name, get_interval_schedule
+from api_clients.pocket_publisher_database_execute import PocketPublisherDatabaseExecute
+from api_clients.pocket_publisher_database_query import PocketPublisherDatabaseQuery
+import time
+import scipy.special
+from datetime import datetime
+
+'''
+Load and update the content quality score in the publisher database
+Note: This was lifted from the Web repo and modified for Prefect 
+'''
+# Setting flow variables
+FLOW_NAME = get_flow_name(__file__)
+
+# move articles to beta queue
+MOVE_TO_BETA_QUEUE_SQL = """
+INSERT IGNORE 
+    INTO content_quality_score_beta_queue (publisher_id, content_id) 
+        SELECT q.publisher_id, q.content_id 
+            FROM content_quality_score_queue q 
+            LEFT JOIN content c ON q.content_id = c.id 
+                  AND q.publisher_id = c.publisher_id
+"""
+
+TRUNCATE_CONTENT_QUALITY_SQL = """
+TRUNCATE TABLE content_quality_score_queue
+"""
+
+DATA_FOR_SCORE_CALCULATION_SQL = """
+SELECT 
+    bq.publisher_id, bq.content_id, pcl.save_cnt, pcl.open_cnt, pcl.favorite_cnt, pcl.share_cnt 
+    from `readitla_pub`.content_quality_score_beta_queue bq 
+    left join `readitla_pub`.publisher_content_lifetime pcl on bq.content_id = pcl.content_id 
+          and bq.publisher_id = pcl.publisher_id limit 100
+"""
+
+INSERT_TO_CONTENT_V1_SQL = """
+INSERT IGNORE 
+    INTO `readitla_pub`.content_quality_score (publisher_id, content_id) 
+    values ({publisher_id}, {content_id})
+"""
+
+INSERT_TO_CONTENT_V2_SQL = """
+INSERT IGNORE 
+    INTO `readitla_pub`.content_quality_score_v2 (publisher_id, content_id) 
+    values ({publisher_id}, {content_id})
+"""
+
+UPDATE_CONTENT_V1_SQL = """
+UPDATE `readitla_pub`.content_quality_score 
+    set save_cnt = {save_cnt}, 
+        open_uv = {open_uv}, 
+        open_dv = {open_dv}, 
+        fav_uv = {fav_uv}, 
+        fav_dv = {fav_dv}, 
+        share_uv = {share_uv}, 
+        share_dv = {share_dv}, 
+        open_score = round({open_score},4), 
+        favorite_score = round({fav_score},4), 
+        share_score = round({share_score},4), 
+        total_score = round({total_score},4) 
+        where publisher_id = {publisher_id}
+              and content_id = {content_id}
+"""
+
+UPDATE_CONTENT_V2_SQL = """
+UPDATE `readitla_pub`.content_quality_score_v2 
+    set save_cnt = {save_cnt}, 
+        open_uv = {open_uv}, 
+        open_dv = {open_dv}, 
+        fav_uv = {fav_uv}, 
+        fav_dv = {fav_dv}, 
+        share_uv = {share_uv}, 
+        share_dv = {share_dv}, 
+        open_score = round({open_score},4), 
+        favorite_score = round({fav_score},4), 
+        share_score = round({share_score},4), 
+        total_score = round({total_score},4) 
+        where publisher_id = {publisher_id}
+              and content_id = {content_id}
+"""
+
+DELETE_BETA_QUEUE_SQL = """
+DELETE FROM `readitla_pub`.content_quality_score_beta_queue 
+    where publisher_id = {publisher_id} 
+    and content_id = {content_id}
+"""
+
+
+with Flow(FLOW_NAME) as flow:
+    # max signed int
+    # 2018-04-11 - We hit the max signed int with our item_ids. This broke tables did not have `unsigned` specified.
+    # The hack we are adding here creates a new content_quality_score table (_v2), fixes the column definitions
+    # and inserts new items into that table.
+    max_content_id = 2147483647
+
+    # score variables
+    loss_multiple = 9
+    inc_beta_var = float(1) / (1 + loss_multiple)
+    open_var = 1
+    fav_var = 5
+    share_var = 7
+
+    # prepare variables
+    first_hour_id = 1357020000
+    current_time = int(time.time())
+    today_hour_id = (current_time / 86400) * 86400
+    current_hour_id = today_hour_id - 86400
+
+    PocketPublisherDatabaseExecute()(
+        query=MOVE_TO_BETA_QUEUE_SQL
+    )
+
+    PocketPublisherDatabaseExecute()(
+        query=TRUNCATE_CONTENT_QUALITY_SQL
+    )
+
+    counter = 0
+
+    # loop through array of publisher_ids
+
+    while True:
+        # query for data needed to calculate score
+        data = PocketPublisherDatabaseQuery()(
+            query=DATA_FOR_SCORE_CALCULATION_SQL
+        )
+
+        if not data:
+            break
+
+        # loop through each row in data
+        for row in data:
+
+            publisher_id = int(row[0])
+            content_id = int(row[1])
+
+            if row[2] is not None and row[3] is not None and row[4] is not None and row[5] is not None:
+                save_cnt = int(row[2])
+                open_cnt = int(row[3])
+                favorite_cnt = int(row[4])
+                share_cnt = int(row[5])
+
+                if save_cnt > 0 or open_cnt > 0 or favorite_cnt > 0 or share_cnt > 0:
+
+                    if content_id > max_content_id:
+                        PocketPublisherDatabaseExecute()(
+                            query=INSERT_TO_CONTENT_V1_SQL.format(publisher_id=publisher_id, content_id=content_id)
+                        )
+                    else:
+                        PocketPublisherDatabaseExecute()(
+                            query=INSERT_TO_CONTENT_V2_SQL.format(
+                                publisher_id=publisher_id, content_id=content_id)
+                        )
+
+                    open_uv = float(open_cnt) + 2.2 + save_cnt * 0.05
+                    open_dv = (float(save_cnt) - open_cnt) + 7.8
+                    open_dv = open_dv if save_cnt >= open_cnt else (open_dv + open_uv) * 4
+                    fav_uv = float(favorite_cnt) + 0.2 + open_cnt * 0.02
+                    fav_dv = (float(open_cnt) - favorite_cnt) + 9.8
+                    fav_dv = fav_dv if open_cnt >= favorite_cnt else (fav_dv + fav_uv) * 4
+                    share_uv = float(share_cnt) + 0.2 + open_cnt * 0.02
+                    share_dv = (float(open_cnt) - share_cnt) + 9.8
+                    share_dv = share_dv if open_cnt >= share_cnt else (share_dv + share_uv) * 4
+
+                    open_score = scipy.special.betaincinv(open_uv, open_dv, inc_beta_var)
+                    fav_score = scipy.special.betaincinv(fav_uv, fav_dv, inc_beta_var)
+                    share_score = scipy.special.betaincinv(share_uv, share_dv, inc_beta_var)
+
+                    total_score = (open_score * open_var + fav_score * fav_var + share_score * share_var) / (
+                            open_var + fav_var + share_var)
+
+                    update_vars = {'save_cnt': save_cnt, 'open_uv': open_uv, 'open_dv': open_dv, 'fav_uv': fav_uv,
+                                   'fav_dv': fav_dv, 'share_uv': share_uv, 'share_dv': share_dv,
+                                   'open_score': open_score, 'fav_score': fav_score, 'share_score': share_score,
+                                   'total_score': total_score, 'publisher_id': publisher_id, 'content_id': content_id}
+
+                    if content_id > max_content_id:
+                        PocketPublisherDatabaseExecute()(
+                            query=UPDATE_CONTENT_V2_SQL.format(**update_vars)
+                        )
+                    else:
+                        PocketPublisherDatabaseExecute()(
+                            query=UPDATE_CONTENT_V1_SQL.format(**update_vars)
+                        )
+
+            PocketPublisherDatabaseExecute()(
+                query=DELETE_BETA_QUEUE_SQL.format(publisher_id=publisher_id, content_id=content_id)
+            )
+
+            counter += 1
+
+if __name__ == "__main__":
+    flow.run()
