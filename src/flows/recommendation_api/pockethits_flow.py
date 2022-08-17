c46a6edb2f6e76461a4528ef7b159990bc244c8d
Matthew Cooper <mcooper@getpocket.com>
2022-08-17 13:58:51 -0700

    revisions after pairing

--- src/flows/recommendation_api/pockethits_flow.py
+++ src/flows/recommendation_api/pockethits_flow.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from prefect import Flow, task
 
 from api_clients.pocket_snowflake_query import PocketSnowflakeQuery, OutputType
@@ -11,10 +10,9 @@ from common_tasks.corpus_candidate_set import (
 )
 from utils import config
 from utils.flow import get_flow_name, get_interval_schedule
-from api_clients.sqs import put_results, RecommendationCandidate, NewTabFeedID
 
 '''
-Builds pocket hits candidate sets for en and de and writes it to SQS and Feature Store
+Builds pocket hits candidate sets for en (and some day de) and writes it to Feature Store
 '''
 
 FLOW_NAME = get_flow_name(__file__)
@@ -23,31 +21,14 @@ POCKETHITS_EN_CANDIDATE_SET_ID = "92411893-ebdb-4a43-ad29-aa79e56e2136"
 
 POCKETHITS_SQL = """
 SELECT
-    s.resolved_id as id,   
-    c.top_domain_name as publisher,
-    s.title,
-    s.scheduled_corpus_item_scheduled_at,
-    s.scheduled_surface_name,
-    s.scheduled_surface_id,
-    s.topic,
-    s.approved_corpus_item_external_id
-FROM scheduled_corpus_items as s
-JOIN content as c
-  ON c.content_id = s.content_id
-WHERE s.scheduled_surface_id = %(SURFACE_GUID)s
-  AND s.scheduled_corpus_item_scheduled_at  BETWEEN (current_date - %(MAX_AGE_DAYS)s) AND current_date 
-ORDER BY s.scheduled_corpus_item_scheduled_at DESC
+    APPROVED_CORPUS_ITEM_EXTERNAL_ID as ID,
+    TOPIC
+FROM "SCHEDULED_CORPUS_ITEMS"
+WHERE SCHEDULED_SURFACE_ID = %(SURFACE_GUID)s
+AND SCHEDULED_CORPUS_ITEM_SCHEDULED_AT BETWEEN DATEADD(day, %(MAX_AGE_DAYS)s, CURRENT_DATE) AND CURRENT_DATE
+QUALIFY row_number() OVER (PARTITION BY APPROVED_CORPUS_ITEM_EXTERNAL_ID ORDER BY SCHEDULED_CORPUS_ITEM_SCHEDULED_AT DESC) = 1
+ORDER BY SCHEDULED_CORPUS_ITEM_SCHEDULED_AT DESC
 """
-
-
-@task()
-def transform_to_candidates(records: dict) -> List[RecommendationCandidate]:
-    # recs api candidate sets don't yet include topic information
-    return [RecommendationCandidate(
-        item_id=rec["ID"],
-        publisher=rec["PUBLISHER"],
-        feed_id=int(NewTabFeedID.en_US)
-    ) for rec in records]
 
 
 @task()
@@ -60,10 +41,11 @@ def transform_to_corpus_items(records: dict) -> List[dict]:
 
 with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30)) as flow:
 
+    # query snowflake for items from pocket hits
     records = PocketSnowflakeQuery()(
         query=POCKETHITS_SQL,
         data={
-            "MAX_AGE_DAYS": 9,
+            "MAX_AGE_DAYS": -9,
             "SURFACE_GUID": "POCKET_HITS_EN_US"
         },
         database=config.SNOWFLAKE_ANALYTICS_DATABASE,
@@ -71,18 +53,16 @@ with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=30)) as flow:
         output_type=OutputType.DICT,
     )
 
-    # SageMaker Feature Store won't be used until we switch in RecAPI
+    # create candidate set
     corpus_items = transform_to_corpus_items(records)
     corpus_items = validate_corpus_items(corpus_items)
     feature_group_record = create_corpus_candidate_set_record(
         id=POCKETHITS_EN_CANDIDATE_SET_ID,
         corpus_items=corpus_items
     )
-    load_feature_record(feature_group_record, feature_group_name=feature_group)
 
-    # write to sqs
-    candidates = transform_to_candidates(records)
-    put_results(POCKETHITS_EN_CANDIDATE_SET_ID, candidates, curated=True)
+    # load candidate set into feature group
+    load_feature_record(feature_group_record, feature_group_name=feature_group)
 
 if __name__ == "__main__":
     flow.run()
