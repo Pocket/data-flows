a8c8f40829aeaa7cb4f1b51ad35704f543871c20
Matthew Cooper <mcooper@getpocket.com>
2022-10-05 13:20:36 -0700

    revising recommended by recency

--- src/flows/recommendation_api/corpus_candidate_sets/topics.py
+++ src/flows/recommendation_api/corpus_candidate_sets/topics.py
@@ -18,7 +18,7 @@ WITH prep as (
     SELECT 
         approved_corpus_item_external_id as "ID", 
         topic as "TOPIC",
-        MAX(reviewed_corpus_item_updated_at) as "REVIEW_TIME" 
+        reviewed_corpus_item_updated_at as "REVIEW_TIME" 
     FROM "ANALYTICS"."DBT"."APPROVED_CORPUS_ITEMS" 
     WHERE REVIEWED_CORPUS_ITEM_UPDATED_AT >= DATEADD('day', -90, current_timestamp())
     AND CORPUS_REVIEW_STATUS = 'recommendation'
@@ -26,14 +26,13 @@ WITH prep as (
     AND SCHEDULED_SURFACE_ID = 'NEW_TAB_EN_US'
     AND NOT is_syndicated
     AND NOT is_collection
-    GROUP BY 1, 2
     
     UNION
     
     SELECT 
         approved_corpus_item_external_id as "ID", 
         topic as "TOPIC",
-        MAX(scheduled_corpus_item_scheduled_at) as "REVIEW_TIME"
+        scheduled_corpus_item_scheduled_at as "REVIEW_TIME"
     FROM "ANALYTICS"."DBT"."SCHEDULED_CORPUS_ITEMS"
     WHERE SCHEDULED_CORPUS_ITEM_SCHEDULED_AT BETWEEN DATEADD('day', -90, current_timestamp()) AND current_timestamp()
     AND CORPUS_ITEM_LOADED_FROM = 'MANUAL'
@@ -41,7 +40,6 @@ WITH prep as (
     AND SCHEDULED_SURFACE_ID = 'NEW_TAB_EN_US'
     AND NOT is_syndicated
     AND NOT is_collection
-    GROUP BY 1, 2
     
     )
 
