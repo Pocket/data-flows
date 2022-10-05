c3876c5e0e090c565143b5c3e78b4b606c1d0f7f
Matthew Cooper <mcooper@getpocket.com>
2022-10-04 17:52:01 -0700

    filtering collections and syndicated items from topic slates

--- src/flows/recommendation_api/corpus_candidate_sets/topics.py
+++ src/flows/recommendation_api/corpus_candidate_sets/topics.py
@@ -24,6 +24,8 @@ WITH prep as (
     AND CORPUS_REVIEW_STATUS = 'recommendation'
     AND TOPIC = %(CORPUS_TOPIC_ID)s
     AND SCHEDULED_SURFACE_ID = 'NEW_TAB_EN_US'
+    AND NOT is_syndicated
+    AND NOT is_collection
     GROUP BY 1, 2
     
     UNION
@@ -37,6 +39,8 @@ WITH prep as (
     AND CORPUS_ITEM_LOADED_FROM = 'MANUAL'
     AND TOPIC = %(CORPUS_TOPIC_ID)s
     AND SCHEDULED_SURFACE_ID = 'NEW_TAB_EN_US'
+    AND NOT is_syndicated
+    AND NOT is_collection
     GROUP BY 1, 2
     
     )
