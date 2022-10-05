92e9705f0bae2b3e7c281e788d79e2171d2d3a2b
Jeshua Irving <jesh@jesh.me>
2022-10-05 16:17:02 -0700

    Try to fix bug where multiple flow runs are not created

--- src/flows/parser_item_html_backfill_load_all_flow.py
+++ src/flows/parser_item_html_backfill_load_all_flow.py
@@ -1,4 +1,4 @@
-import math
+from datetime import datetime
 
 import numpy as np
 import prefect
@@ -6,16 +6,16 @@ from prefect import Flow, task, Parameter
 from prefect.tasks.aws import S3List
 from prefect.tasks.prefect import create_flow_run
 
+from flows.parser_item_html_backfill_load_file_flow import FLOW_NAME as file_flow_name
 from utils import config
 from utils.flow import get_flow_name
-from flows.parser_item_html_backfill_load_file_flow import FLOW_NAME as file_flow_name
 
 # Setting flow variables
 FLOW_NAME = get_flow_name(__file__)
 S3_BUCKET = 'pocket-data-items'
 SOURCE_PREFIX = 'article/backfill-html-filesplit/'
-NUM_FILES_PER_RUN = 1000
-NUM_WORKERS = 5
+NUM_FILES_PER_RUN = 10
+NUM_WORKERS = 2
 
 
 @task()
@@ -39,10 +39,17 @@ def get_source_keys(num_files: int) -> [str]:
 @task()
 def process(keys, num_workers: int):
     logger = prefect.context.get("logger")
-    for keys in np.array_split(keys, num_workers):
-        logger.info(f"Queuing worker for keys: {*keys,}.")
+    idx = 0
+    name_prefix = f'{FLOW_NAME}-{datetime.now().strftime("%m%d%Y-%H:%M:%S")}'
+    chunks = np.array_split(keys, num_workers)
+    for chunk in chunks:
+        logger.info(f"Queuing worker for keys: {*chunk,}.")
+        # testing adding run_name to the parameters because without it prefect is kicking off the same flow run many
+        # times with different parameters and only the last one is succeeding. The hope is that with a name it will
+        # create a new flow run.
         create_flow_run.run(flow_name=file_flow_name, project_name=config.PREFECT_PROJECT_NAME,
-                            parameters={"keys": keys.tolist()})
+                            parameters={"keys": chunk.tolist()}, run_name=f'{name_prefix}-{idx}')
+        idx += 0
 
 
 with Flow(FLOW_NAME) as flow:
