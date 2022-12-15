7a8d5ba6e4073d1cef2655df0244652d740a9ffb
Mathijs Miermans <m.miermans@gmail.com>
2022-12-15 10:02:47 -0800

    Chore(CorpusItemEngagement): Cleanup flow

--- src/flows/recommendation_api/corpus_item_engagement.py
+++ src/flows/recommendation_api/corpus_item_engagement.py
@@ -10,35 +10,28 @@ from utils.flow import get_flow_name, get_interval_schedule
 FLOW_NAME = get_flow_name(__file__)
 
 BASE_QUERY = """
-with trailing_engagement as (
-    SELECT
-        'HOME' as RECOMMENDATION_SURFACE_ID,
-        e.CORPUS_SLATE_CONFIGURATION_ID,
-        e.CORPUS_ITEM_ID,
+SELECT
+    'HOME' as RECOMMENDATION_SURFACE_ID,  -- TDP-192: Get surface id from Snowflake to support surfaces other than Home.
+    CORPUS_SLATE_CONFIGURATION_ID,
+    CORPUS_ITEM_ID,
 
-        -- aggregate impressions
-        sum(case when happened_at >= CURRENT_DATE      then impression_count else 0 end) as trailing_1_day_impressions,
-        sum(case when happened_at >= CURRENT_DATE - 6  then impression_count else 0 end) as trailing_7_day_impressions,
-        sum(case when happened_at >= CURRENT_DATE - 13 then impression_count else 0 end) as trailing_14_day_impressions,
-        sum(case when happened_at >= CURRENT_DATE - 20 then impression_count else 0 end) as trailing_21_day_impressions,
-        sum(case when happened_at >= CURRENT_DATE - 27 then impression_count else 0 end) as trailing_28_day_impressions,
+    -- aggregate impressions
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE      then impression_count else 0 end) as TRAILING_1_DAY_IMPRESSIONS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 6  then impression_count else 0 end) as TRAILING_7_DAY_IMPRESSIONS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 13 then impression_count else 0 end) as TRAILING_14_DAY_IMPRESSIONS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 20 then impression_count else 0 end) as TRAILING_21_DAY_IMPRESSIONS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 27 then impression_count else 0 end) as TRAILING_28_DAY_IMPRESSIONS,
 
-        -- aggregate opens
-        sum(case when happened_at >= CURRENT_DATE      then open_count else 0 end) as trailing_1_day_opens,
-        sum(case when happened_at >= CURRENT_DATE - 6  then open_count else 0 end) as trailing_7_day_opens,
-        sum(case when happened_at >= CURRENT_DATE - 13 then open_count else 0 end) as trailing_14_day_opens,
-        sum(case when happened_at >= CURRENT_DATE - 20 then open_count else 0 end) as trailing_21_day_opens,
-        sum(case when happened_at >= CURRENT_DATE - 27 then open_count else 0 end) as trailing_28_day_opens
+    -- aggregate opens
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE      then open_count else 0 end) as TRAILING_1_DAY_OPENS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 6  then open_count else 0 end) as TRAILING_7_DAY_OPENS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 13 then open_count else 0 end) as TRAILING_14_DAY_OPENS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 20 then open_count else 0 end) as TRAILING_21_DAY_OPENS,
+    sum(case when HAPPENED_AT_DAY >= CURRENT_DATE - 27 then open_count else 0 end) as TRAILING_28_DAY_OPENS
 
-    FROM "ANALYTICS"."DBT_MMIERMANS"."CORPUS_RECOMMENDATION_ENGAGEMENT_BY_ITEM_BY_DAY" e
-    WHERE e.happened_at >= CURRENT_DATE - 27
-    GROUP BY 1, 2, 3
-)
-
-SELECT 
-    
-    trailing_engagement.*
-FROM trailing_engagement;
+FROM "ANALYTICS"."DBT"."CORPUS_RECOMMENDATION_ENGAGEMENT_BY_ITEM_BY_DAY"
+WHERE HAPPENED_AT_DAY >= CURRENT_DATE - 27
+GROUP BY 1, 2, 3
 """
 
 
@@ -50,14 +43,13 @@ def transform_df(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-# Ideally, this flow would be triggered after Dbt model used in the above query is updated.
+# TODO: Ideally, this flow would be triggered after Dbt model used in the above query is updated, instead of every hour.
 with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=60)) as flow:
-    feature_group = Parameter("feature group", default=f"{config.ENVIRONMENT}-corpus-engagement-v1")
-
     snowflake_result = PocketSnowflakeQuery()(query=BASE_QUERY)
 
     transformed_df = transform_df(snowflake_result)
-    dataframe_to_feature_group(df=transformed_df, feature_group_name=feature_group)
+
+    dataframe_to_feature_group(transformed_df, feature_group_name=f"{config.ENVIRONMENT}-corpus-engagement-v1")
 
 if __name__ == "__main__":
     flow.run()
