e9c062c022421ac876180fea6fcd55a05e419c90
jesh <jesh@getpocket.com>
2022-12-19 17:26:03 -0800

    Cleanup command

--- src/flows/data_retention/development_snowflake_schemas.py
+++ src/flows/data_retention/development_snowflake_schemas.py
@@ -5,9 +5,11 @@ Reference: https://getpocket.atlassian.net/wiki/spaces/CP/pages/2703949851/Snowf
 from prefect import Flow, task, unmapped
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import CronClock
+from prefect.tasks.snowflake import SnowflakeQuery
 
 from api_clients.pocket_snowflake_query import PocketSnowflakeQuery
 from utils.flow import get_flow_name
+from utils.config import SNOWFLAKE_DEFAULT_DICT
 
 FLOW_NAME = get_flow_name(__file__)
 DATABASE = "DEVELOPMENT"
@@ -23,14 +25,7 @@ SELECT catalog_name as database,
        last_altered
 FROM information_schema.schemata
 WHERE created < DATEADD("day", -89, CURRENT_TIMESTAMP())
-AND schema_name not in ('DBT_SBISWAS', 'PUBLIC', 'DBT', 'DBT_STAGING');
-"""
-"""
-We are excluding schema `DBT_SBISWAS` as its being actively used for delivering Android push notifications experiments.
-Kirill will be doing an experiment to explore doing this work our DBT workflow and as a result putting this data in our
-ANALYTICS database. Once we have results on this experiment we will decide next steps for this schema exclusion.
-
-https://pocket.slack.com/archives/C03E28D1GUD/p1653672644243659?thread_ts=1653582347.792469&cid=C03E28D1GUD
+AND schema_name not in ('PUBLIC');
 """
 
 
@@ -40,13 +35,15 @@ We added the fully qualified schema name to be 100% confident that we are deleti
 """
 
 
+def query():
+    return SnowflakeQuery(**SNOWFLAKE_DEFAULT_DICT, database=DATABASE)
+
+
 @task()
-def delete_schema(row: dict[str, str], ex: PocketSnowflakeQuery):
-    return ex(query=DROP_SCHEMA_SQL, data={"schema_name": row['schema_name']})
+def drop_schema(row: dict[str]):
+    return query().run(data={'schema_name': row[0]}, query=DROP_SCHEMA_SQL)
 
 
 with Flow(FLOW_NAME) as flow:
-    executor = PocketSnowflakeQuery(database=DATABASE)
-    results = executor(query=GET_SCHEMAS_SQL)
-    delete_schema.map(results, unmapped(executor))
-
+    schemas = query().run(query=GET_SCHEMAS_SQL)
+    drop_schema.map(schemas)
