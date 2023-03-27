a7cfcc563ed72e8d7216057d8a81e78824374fa1
Braun <breyes@mozilla.com>
2023-03-27 14:13:13 -0500

    style(data-products): black formatting

--- data-products/tests/orchestration/unit/test_main_orchestration_flow.py
+++ data-products/tests/orchestration/unit/test_main_orchestration_flow.py
@@ -6,18 +6,20 @@ import pytest
 from prefect.testing.utilities import prefect_test_harness
 from unittest.mock import patch
 
+
 @pytest.fixture(autouse=True, scope="session")
 def prefect_test_fixture():
     with prefect_test_harness():
         yield
 
-@patch('src.orchestration.main_orchestration_flow.get_run_logger')
+
+@patch("src.orchestration.main_orchestration_flow.get_run_logger")
 def test_orchestrate_flows(mock):
     orchestrate_flows.fn()
     mock.assert_called_once()
 
-@patch('src.orchestration.main_orchestration_flow.orchestrate_flows')
+
+@patch("src.orchestration.main_orchestration_flow.orchestrate_flows")
 def test_main_orchestration_flow(mock):
     main_orchestration_flow()
     mock.assert_called_once()
-
