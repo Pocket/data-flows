0db719593350233b98c8a932e5c655417e50ee81
Mathijs Miermans <m.miermans@gmail.com>
2023-04-04 19:35:02 -0700

    fix(NewTab): Remove dummy data and check for empty df

--- src/flows/recommendation_api/ff_new_tab_aggregated_engagement.py
+++ src/flows/recommendation_api/ff_new_tab_aggregated_engagement.py
@@ -1,7 +1,7 @@
 from typing import List, Union
 
 import pandas as pd
-from prefect import Flow, task
+from prefect import Flow, task, case
 from prefect.tasks.gcp.bigquery import BigQueryTask
 
 from api_clients.pocket_snowflake_query import PocketSnowflakeQuery
@@ -21,7 +21,6 @@ EXPORT_FIREFOX_TELEMETRY_SQL = """
             AND normalized_country_code IS NOT NULL
             AND array_length(tiles) >= 1
         )
-        (
         SELECT
             CAST(flattened_tiles.id AS INT64) AS TILE_ID,
             FORMAT_DATETIME("%Y-%m-%dT%H:%M:%SZ", CURRENT_DATETIME()) as UPDATED_AT,  -- Feature Store requires ISO 8601 time format
@@ -40,27 +39,6 @@ EXPORT_FIREFOX_TELEMETRY_SQL = """
         CROSS JOIN UNNEST(impressions_data.tiles) AS flattened_tiles
         GROUP BY TILE_ID
         HAVING TRAILING_1_DAY_IMPRESSIONS > 1000 -- removes content that isn't actively served but still gets impressions.
-        )
-        
-        -- Dummy data from example Snowplow event 8c608112-b1c4-4354-b66b-9f00324e064f
-        -- Remove once we get 
-        union all select 1018311686533393, '2023-04-04T10:02:00Z', 1000, 93, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2336135188504018, '2023-04-04T10:02:00Z', 1000, 18, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 1196919788541269, '2023-04-04T10:02:00Z', 1000, 69, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 3696711462613678, '2023-04-04T10:02:00Z', 1000, 78, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 3899469774231035, '2023-04-04T10:02:00Z', 1000, 35, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2137436018802803, '2023-04-04T10:02:00Z', 1000, 3, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2516923037524350, '2023-04-04T10:02:00Z', 1000, 50, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2213560505990250, '2023-04-04T10:02:00Z', 1000, 50, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2406475520295944, '2023-04-04T10:02:00Z', 1000, 44, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 3327856852376222, '2023-04-04T10:02:00Z', 1000, 22, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 903421925709054, '2023-04-04T10:02:00Z', 1000, 54, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 4127678838378836, '2023-04-04T10:02:00Z', 1000, 36, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 768277328123901, '2023-04-04T10:02:00Z', 1000, 1, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2141616218975497, '2023-04-04T10:02:00Z', 1000, 97, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2872260490206165, '2023-04-04T10:02:00Z', 1000, 65, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 2594624165134442, '2023-04-04T10:02:00Z', 1000, 42, 0, 0, 0, 0, 0, 0, 0, 0
-        union all select 3796701044018254, '2023-04-04T10:02:00Z', 1000, 54, 0, 0, 0, 0, 0, 0, 0, 0
     """
 
 
@@ -94,6 +72,11 @@ def df_drop(df: pd.DataFrame, columns: Union[str, List[str]]) -> pd.DataFrame:
     return df.drop(columns=columns)
 
 
+@task()
+def df_is_empty(df: pd.DataFrame) -> bool:
+    return df.empty
+
+
 with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=15)) as flow:
     # Get telemetry from BigQuery
     df_telemetry = BigQueryTask()(
@@ -107,15 +90,18 @@ with Flow(FLOW_NAME, schedule=get_interval_schedule(minutes=15)) as flow:
         data={'tile_ids': df_column_to_list(df_telemetry, column_name='TILE_ID')},
     )
 
-    # Combine the BigQuery and Snowflake results on TILE_ID.
-    df_keyed_telemetry = pd_merge(df_telemetry, df_corpus_item_keys)
-    # Drop TILE_ID now we no longer need it, to match the dataframe columns with the feature group.
-    df_keyed_telemetry = df_drop(df_keyed_telemetry, columns=['TILE_ID'])
+    # If none of the tileIds exist in Snowflake, then there's nothing to do. This should only happen while the new API
+    # is under development, and there are days without any NewTab impressions on content served from Recommendation API.
+    with case(df_is_empty(df_corpus_item_keys), False):
+        # Combine the BigQuery and Snowflake results on TILE_ID.
+        df_keyed_telemetry = pd_merge(df_telemetry, df_corpus_item_keys)
+        # Drop TILE_ID now we no longer need it, to match the dataframe columns with the feature group.
+        df_keyed_telemetry = df_drop(df_keyed_telemetry, columns=['TILE_ID'])
 
-    dataframe_to_feature_group(
-        dataframe=df_keyed_telemetry,
-        feature_group_name=f"{config.ENVIRONMENT}-corpus-engagement-v1"
-    )
+        dataframe_to_feature_group(
+            dataframe=df_keyed_telemetry,
+            feature_group_name=f"{config.ENVIRONMENT}-corpus-engagement-v1"
+        )
 
 if __name__ == "__main__":
     flow.run()
