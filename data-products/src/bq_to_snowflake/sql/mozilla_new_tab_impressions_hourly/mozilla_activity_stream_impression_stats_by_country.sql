cf2d515b2269ab250bc786aa9c4edf9171c19f93
Braun <breyes@mozilla.com>
2023-05-10 16:46:08 -0500

    feat(bq_to_snowflake): initial commit

---
+++ data-products/src/bq_to_snowflake/sql/mozilla_new_tab_impressions_hourly/mozilla_activity_stream_impression_stats_by_country.sql
@@ -0,0 +1,57 @@
+-- original path/file: dags/mozilla_stats_hourly_bq/mozilla_stats_by_source_country_export.sql
+
+WITH deduplicated as (
+  SELECT *
+  FROM (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY date(submission_timestamp), document_id order by submission_timestamp) AS row_number
+    FROM `moz-fx-data-shared-prod.activity_stream_live.impression_stats_v1`
+    WHERE EXTRACT(date FROM submission_timestamp) BETWEEN DATE_ADD('{{ ds }}', INTERVAL -1 DAY) AND '{{ ds }}'
+  )
+  WHERE row_number = 1
+),
+
+impression_data AS (
+    SELECT s.*
+    FROM deduplicated AS s
+    WHERE loaded IS NULL --don't include loaded ping
+        AND array_length(tiles) >= 1 --make sure data is valid/non-empty
+        AND (source IS NULL or source != 'https://newtab.firefoxchina.cn/newtab/as/activity-stream.html') --exclude custom New Tab page for FX China
+        AND (page IS NULL or page != 'https://newtab.firefoxchina.cn/newtab/as/activity-stream.html') --exclude custom New Tab page for FX China
+),
+
+flattened_impression_data AS ( --need this step to filter out >2 clicks from a given client on the same tile within 1 second
+    SELECT
+        UNIX_SECONDS(submission_timestamp) AS submission_timestamp, --truncate timestamp to seconds
+        impression_id AS client_id, --client_id renamed to impression_id in GCP
+        flattened_tiles.id AS tile_id,
+        IFNULL(flattened_tiles.pos, alt_pos) AS position, --the 3x1 layout has a bug where we need to use the position of each element in the tiles array instead of the actual pos field
+        source,
+        locale,
+        normalized_country_code AS country,
+        SUM(CASE WHEN click IS NULL AND block IS NULL AND pocket IS NULL THEN 1 ELSE 0 END) AS impressions,
+        SUM(CASE WHEN click IS NOT NULL THEN 1 ELSE 0 END) AS clicks,
+        SUM(CASE WHEN pocket IS NOT NULL THEN 1 ELSE 0 END) AS pocketed,
+        SUM(CASE WHEN block IS NOT NULL THEN 1 ELSE 0 END) AS blocked
+    FROM impression_data
+    CROSS JOIN UNNEST(impression_data.tiles) AS flattened_tiles
+    WITH OFFSET AS alt_pos
+    GROUP BY 1, 2, 3, 4, 5, 6, 7
+)
+
+SELECT
+    DATE(TIMESTAMP_SECONDS(submission_timestamp)) AS activity_date,
+    tile_id,
+    position,
+    source,
+    locale,
+    country,
+    SUM(impressions) AS impressions,
+    SUM(clicks) AS clicks,
+    SUM(pocketed) AS pocketed,
+    SUM(blocked) AS blocked
+FROM flattened_impression_data
+WHERE clicks < 3
+GROUP BY 1, 2, 3, 4, 5, 6
+ORDER BY 1, 2, 3, 4, 5, 6;
---
+++ data-products/src/bq_to_snowflake/sql/mozilla_new_tab_prefs/mozilla_activity_stream_prefs.sql
@@ -0,0 +1,27 @@
+-- original path/file: dags/mozilla_stats_twice_daily_bq/prefs_export.sql
+
+WITH deduplicated as (
+  SELECT *
+  FROM (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY date(submission_timestamp), document_id order by submission_timestamp) AS row_number
+    FROM `moz-fx-data-shared-prod.activity_stream_live.sessions_v1`
+    WHERE EXTRACT(date FROM submission_timestamp) BETWEEN DATE_ADD('{{ ds }}', INTERVAL -1 DAY) AND '{{ ds }}'
+  )
+  WHERE row_number = 1
+)
+
+select
+    DATE(submission_timestamp) AS activity_date,
+    count(distinct client_id) AS users,
+    count(distinct case when user_prefs & 4 != 4 THEN client_id end) AS pocket_disabled_users,
+    count(distinct case when user_prefs & 32 != 32 THEN client_id end) AS spoc_disabled_users,
+    count(distinct case when user_prefs & 4 = 4 AND user_prefs & 32 = 32 THEN client_id end) AS spoc_eligible_users
+from deduplicated
+WHERE normalized_country_code = 'US'
+    and locale LIKE 'en%'
+    and release_channel = 'release' --this is only beta for now, this is why user numbers are so low
+    and (version like '6%' OR version like '7%' OR version like '8%' OR version like '9%' OR version like '10%')
+group by 1
+order by 1;
