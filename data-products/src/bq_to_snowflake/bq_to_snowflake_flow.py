2c8851b53c4bcd49f77ab3679d070336db4851e5
Braun <breyes@mozilla.com>
2023-05-12 12:52:16 -0500

    Revert "feat(bq_to_snowflake): commit of basic skeleton for subflows"
    This reverts commit 3a676905ed219182753eb52c4b1e9a2792e68a32.

--- data-products/src/bq_to_snowflake/bq_to_snowflake_flow.py
+++ data-products/src/bq_to_snowflake/bq_to_snowflake_flow.py
@@ -1,36 +1,21 @@
 from prefect import flow, task
-from glob import glob
-import os
-
-SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 @task()
-def bq_staging(file_path: str):
-    return "Hello World"
+def bq_staging():
+    pass
 
 @task()
-def bq_extract(input):
-    return "Hello World"
+def bq_extract():
+    pass
 
 @task()
-def snowflake_load(input):
-    return "Hello World"
+def snowflake_load():
+    pass
 
 @flow()
-def bq_to_snowflake_etl(file_path: str):
-    stg = bq_staging(file_path)
-    ex = bq_extract(stg)
-    snowflake_load(ex)
+def bq_to_snowflake_etl():
+    pass
 
 @flow()
 def bq_to_snowflake():
-    """Main Prefect flow for orchestrating the ingestion of
-    specific Big Query data extractions into Snowflake.  Per author
-    of the original Airflow DAG, each SQL file can run in a concurrent
-    workflow.
-    """
-    for f in glob(os.path.join(SCRIPT_PATH, "sql/**/*.sql")):
-        bq_to_snowflake_etl(f)
-
-if __name__ == "__main__":
-    bq_to_snowflake()
+    pass
