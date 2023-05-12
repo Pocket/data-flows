a89f2ea27007d0ce31f39465661a6db70ab20a00
Braun <breyes@mozilla.com>
2023-05-12 12:49:31 -0500

    Revert "feat(bq_to_snowflake): another example commit for async execution of subflows"
    This reverts commit 2fb8335534fa5fb47a5b5c6eeab8887cd51dfbe4.

--- data-products/src/bq_to_snowflake/mozilla_new_tab_impressions_daily_flow.py
+++ data-products/src/bq_to_snowflake/mozilla_new_tab_impressions_daily_flow.py
@@ -1,17 +1,16 @@
 from flow_components import subflow_factory
 from prefect import flow
-import asyncio
 
 
 @flow()
-async def mozilla_new_tab_impressions_daily():
+def mozilla_new_tab_impressions_daily():
     """Main Prefect flow for orchestrating the ingestion of
     specific Big Query data extractions into Snowflake.  Per author
     of the original Airflow DAG, each SQL file can run in a concurrent
     workflow.
     """
-    await subflow_factory("mozilla_new_tab_impressions_daily")
+    subflow_factory("mozilla_new_tab_impressions_daily")
 
 
 if __name__ == "__main__":
-    asyncio.run(mozilla_new_tab_impressions_daily())
+    mozilla_new_tab_impressions_daily()
