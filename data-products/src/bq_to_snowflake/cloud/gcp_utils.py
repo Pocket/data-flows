73fb2aba6cc329537da885ab9dff2e2af33094a2
Braun <breyes@mozilla.com>
2023-05-12 12:51:47 -0500

    Revert "feat(bq_to_snowflake): final commit for demo purposes...still in progress for release"
    This reverts commit 97383d7633ef8e34d7660989845dd7657fc46256.

--- data-products/src/bq_to_snowflake/cloud/gcp_utils.py
+++ data-products/src/bq_to_snowflake/cloud/gcp_utils.py
@@ -1,28 +1,28 @@
 from common.settings import NestedSettings, Settings
 from prefect.blocks.fields import SecretDict
 from prefect_gcp import BigQueryWarehouse, GcpCredentials
-from pathlib import Path
-from typing import Optional
 
 
 class GcpCredSettings(NestedSettings):
-    service_account_info: Optional[SecretDict]
-    service_account_file: Optional[Path]
+    service_account_info: SecretDict
+    project: str
 
 
 class GcpSettings(Settings):
     gcp_credentials: GcpCredSettings
 
 
+SETTINGS = GcpSettings()  # type: ignore
+
+
 class PktGcpCredentials(GcpCredentials):
-    def __init__(self, **data):
-        settings = GcpSettings() # type: ignore
+    def __init__(self):
         super().__init__(
-            service_account_info=settings.gcp_credentials.service_account_info,
-            service_account_file=settings.gcp_credentials.service_account_file,
+            service_account_info=SETTINGS.gcp_credentials.service_account_info,
+            project=SETTINGS.gcp_credentials.project,
         )
 
 
 class PktBigQueryWarehouse(BigQueryWarehouse):
-    def __init__(self, **data):
+    def __init__(self):
         super().__init__(gcp_credentials=PktGcpCredentials())
--- data-products/src/bq_to_snowflake/flow_components.py
+++ data-products/src/bq_to_snowflake/flow_components.py
@@ -1,64 +1,36 @@
-import json
 import os
-
-from cloud.gcp_utils import PktBigQueryWarehouse, PktGcpCredentials
-from databases.snowflake_utils import PktSnowflakeConnector
-
-# from pendulum import now as pd_now
+import json
 from prefect import flow, task
-from prefect_gcp.bigquery import bigquery_query
-from prefect_snowflake.database import snowflake_query
-
-# from pathlib import Path
-
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
-MOZ_DATA_PROJECT = os.getenv("MOZ_DATA_PROJECT", "moz-fx-data-shared-prod")
 
 
 @task()
-def create_bq_schema():
-    with PktBigQueryWarehouse() as w:
-        result = w.execute("CREATE SCHEMA IF NOT EXISTS mozilla")
-        return result
+def bq_staging(file_path: str):
+    return "Hello World"
 
 
 @task()
 def bq_extract(input):
-    print(input)
-    return input
+    return "Hello World"
+
+
+@task()
+def snowflake_load(input):
+    return "Hello World"
 
 
 @flow()
-def bq_to_snowflake(subflow_set_def: dict):
-    # ds = create_bq_schema()
-    stg = bigquery_query(
-        gcp_credentials=PktGcpCredentials(),
-        # query=Path(
-        #     os.path.join(SCRIPT_PATH, subflow_set_def["source_sql_path"])
-        # ).read_text(),
-        query="""select 1 from `moz-fx-data-shared-prod.activity_stream_live.sessions_v1`
-        where submission_timestamp > '2023-05-11'
-        limit 1;""",  # noqa: E501
-        # dataset=f'{subflow_set_def["schema_name"]}',
-        # table=f'{subflow_set_def["seq_id"]}_{int(pd_now(tz="utc").timestamp())}',
-        # job_config={
-        #     "create_disposition": "CREATE_IF_NEEDED",
-        #     "write_disposition": "WRITE_TRUNCATE",
-        # },
-        #  wait_for=[ds] # type: ignore
-    )
-    ex = bq_extract(stg)
-    snowflake_query(
-        snowflake_connector=PktSnowflakeConnector(),
-        query="select 1;",
-        wait_for=[ex],  # type: ignore
-    )
+def bq_to_snowflake():
+        stg = bq_staging("test")
+        ex = bq_extract(stg)
+        snowflake_load(ex)
 
 
 def subflow_factory(group_id: str):
-    with open(os.path.join(SCRIPT_PATH, "config/bq_to_snowflake.json")) as f:
+    with open("src/bq_to_snowflake/config/bq_to_snowflake.json") as f:
         groups = json.load(f)
     group = groups[group_id]
     for s in group["subflow_set_defs"]:
-        bq_to_snowflake(s)
+         bq_to_snowflake()
+    
