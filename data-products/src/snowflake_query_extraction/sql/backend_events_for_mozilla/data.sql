46051405a70a912d488bb6fb5999e1f6a357fdcd
Braun <breyes@mozilla.com>
2023-06-12 23:35:54 -0500

    feat(data-products): updating sql query for extraction of nimbus events

--- data-products/src/snowflake_query_extraction/sql/backend_events_for_mozilla/data.sql
+++ data-products/src/snowflake_query_extraction/sql/backend_events_for_mozilla/data.sql
@@ -1,7 +1,59 @@
-select *
+select collector_tstamp as submission_timestamp,
+derived_tstamp as event_timestamp,
+app_id as app_id,
+object_construct(
+  'metadata', object_construct(
+    'geo', object_construct(
+      'city', geo_city, 
+      'country', geo_country),
+    'user_agent', object_construct(
+      'browser', CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:useragentFamily::string, 
+      'os', CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:osFamily::string,
+      'version', CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:useragentMajor::string || CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:useragentMinor::string)
+    )
+) as metadata,
+object_construct(
+  'client_info', object_construct(
+    'app_build', nvl(CONTEXTS_COM_SNOWPLOWANALYTICS_MOBILE_APPLICATION_1[0]:version::string, CONTEXTS_COM_POCKET_API_USER_1[0]:client_version::string),
+    'client_id', CONTEXTS_COM_POCKET_USER_1[0]:hashed_guid::string,
+    'user_id', CONTEXTS_COM_POCKET_USER_1[0]:hashed_user_id::string,
+    'locale', br_lang,
+    'os', CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:osFamily::string,
+    'os_version', CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:osMajor::string || CONTEXTS_COM_SNOWPLOWANALYTICS_SNOWPLOW_UA_PARSER_CONTEXT_1[0]:osMinor::string
+    )
+) as client_info,
+'event' as event_category,
+event_name,
+object_construct(
+    'api_id', COALESCE(CONTEXTS_COM_POCKET_API_USER_1[0]:api_id::int, 0),
+    'page_url_host', page_urlhost,
+    'page_url_path', page_urlpath,
+    'page_url_query', page_urlquery,
+    'bottom_ui_type', CONTEXTS_COM_POCKET_UI_1[0]:type::string,
+    'bottom_ui_component_detail', CONTEXTS_COM_POCKET_UI_1[0]:component_detail::string,
+    'bottom_ui_identifier', CONTEXTS_COM_POCKET_UI_1[0]:identifier::string,
+    'bottom_ui_index', CONTEXTS_COM_POCKET_UI_1[0]:index::integer
+) as event_extras
 from {database_name}.{schema_name}.{table_name}
 where {offset_key} > '{current_offset}'
 and {offset_key} <= '{new_offset}'
 and {offset_key} > current_timestamp - interval '24 hours'
+and 
+  (event_name = 'list_item_update' and UNSTRUCT_EVENT_COM_POCKET_LIST_ITEM_UPDATE_1:trigger::string = 'save')
+  or
+  (event_name = 'content_open')
+  or
+  (event_name = 'app_open')
+  or
+  (event_name = 'page_view' and
+    (
+    CONTEXTS_COM_POCKET_USER_1[0]:hashed_user_id::string is not null
+    or page_urlpath like any ('/explore%' , '/__/explore%', '/__-__/explore%')
+    or page_urlpath like any ('/collections%' , '/__/collections%', '/__-__/collections%')
+    )
+  )
+and app_id not like '%-dev' 
+and app_id like 'pocket-%'
+qualify row_number () over (partition by event_id order by collector_tstamp) = 1
 
 
