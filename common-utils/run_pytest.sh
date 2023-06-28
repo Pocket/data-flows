35c7ce03aa93d39738c325d14dd22b5bc985ac40
Braun <breyes@mozilla.com>
2023-06-28 13:27:16 -0500

    fix(common-utils): added helper script for running tests per deployment type and added logic to .circleci/workflows.yml

---
+++ common-utils/run_pytest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Bash script for running unit test suite once per deployment type
+
+set -e
+
+for env in "dev" "staging" "main"
+do
+    export DF_CONFIG_DEPLOYMENT_TYPE=$env
+    echo "Running unit tests for deployment type: $env ..."
+    poetry run python -m pytest -s -vvv --cov=src --cov-report term-missing --cov-fail-under=100
+done
