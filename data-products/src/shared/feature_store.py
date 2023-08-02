012ec2dbb9111d547820b6e4984f0ca6783eda1e
Mathijs Miermans <mmiermans@users.noreply.github.com>
2023-08-02 14:20:51 -0700

    feat: migrate new tab feature store engagement (#228)
    * feat(data-products): initial code to migration fx new tab migrations
    
    * feat(data-products): load dataframe using feature store
    
    * fix: Successfully queries from BigQuery and Snowflake
    
    * feat(feature_store): convert to asyncio
    
    * chore: add comments
    
    * fix: linting
    
    * chore: add tests for feature_store.py
    
    * fix: lint error
    
    * fix: bump data-products version
    
    * feat: add FlowSpec
    
    * fix: add FlowSpec to both flows
    
    * fix: add gcp_stages env and remove feature group FlowSpec
    
    * feat: add put_record feature group access
    
    * fix: prettier error
    
    * fix: missing CommonSettings import
    
    * chore: update flow & deployment names
    
    * feat: parallelize dataframe_to_feature_group
    
    Uses Prefect for retrying, and remove custom retry logic and semaphore.
    
    * chore: rename task ingest_dataframe
    
    * fix: linter
    
    ---------
    
    Co-authored-by: Braun <breyes@mozilla.com>

---
+++ data-products/src/shared/feature_store.py
@@ -0,0 +1,125 @@
+import asyncio
+from asyncio import run
+from typing import NamedTuple
+
+import aioboto3
+import pandas as pd
+from common.settings import Settings, CommonSettings
+from prefect import get_run_logger, flow, task
+from prefect_dask import DaskTaskRunner
+
+CS = CommonSettings()
+
+
+class FeatureGroupSettings(Settings):
+    """Settings for Sagemaker Feature Groups."""
+
+    feature_group_prefix: str = "production" if CS.is_production else "development"
+    corpus_engagement_feature_group_name: str = (
+        f"{feature_group_prefix}-corpus-engagement-v1"
+    )
+
+
+@flow(
+    validate_parameters=False,
+    task_runner=DaskTaskRunner(
+        cluster_kwargs={"n_workers": 4, "threads_per_worker": 1}
+    ),
+)
+async def dataframe_to_feature_group(
+    dataframe: pd.DataFrame, feature_group_name: str, concurrency_limit: int = 100
+):
+    """
+    Update SageMaker feature group.
+
+    :param dataframe: the data in a dataframe to upload to the feature group
+    :param feature_group_name: the name of the feature group to upload the data to
+    :param concurrency_limit: Maximum number of concurrent HTTP requests to make to the FeatureGroup.
+    """
+    # Split dataframe into chunks with at most concurrency_limit rows.
+    tasks = [
+        ingest_dataframe(
+            dataframe=dataframe[i : i + concurrency_limit],
+            feature_group_name=feature_group_name,
+        )
+        for i in range(0, dataframe.shape[0], concurrency_limit)
+    ]
+
+    logger = get_run_logger()
+    logger.info(
+        f"Ingesting {len(dataframe)} records in {len(tasks)} chunks into {feature_group_name}"
+    )
+    await asyncio.gather(*tasks)
+
+
+INGEST_ROWS_RETRIES = 2
+
+
+@task(retries=INGEST_ROWS_RETRIES)
+async def ingest_dataframe(dataframe: pd.DataFrame, feature_group_name: str):
+    async with aioboto3.Session().client(
+        "sagemaker-featurestore-runtime"
+    ) as featurestore:
+        tasks = [
+            ingest_row(
+                row=row,
+                featurestore=featurestore,
+                feature_group_name=feature_group_name,
+            )
+            for row in dataframe.itertuples(index=False)
+        ]
+
+        await asyncio.gather(*tasks)
+
+
+async def ingest_row(
+    row: NamedTuple,
+    feature_group_name: str,
+    featurestore,
+):
+    """Ingest a single Dataframe row into FeatureStore.
+
+    Args:
+        :param row: current row that is being ingested
+        :param feature_group_name: name of the Feature Group.
+        :param featurestore: aioboto3 client for sagemaker-featurestore-runtime
+    """
+    await featurestore.put_record(
+        FeatureGroupName=feature_group_name,
+        Record=[
+            {
+                "FeatureName": column_name,
+                "ValueAsString": str(getattr(row, column_name)),
+            }
+            for column_name in row._fields  # Seems this is a common way to iterate over a NamedTuple's fields?
+        ],
+    )
+
+
+if __name__ == "__main__":
+    run(
+        dataframe_to_feature_group(
+            dataframe=pd.DataFrame(
+                [
+                    {
+                        "KEY": "NEW_TAB_FR_FR/41081474-0b70-5cd8-8177-f495b371030b/eca15def-ec21-49c4-b828-5014815b1918",
+                        "UPDATED_AT": "2023-07-28T18:52:17Z",
+                        "RECOMMENDATION_SURFACE_ID": "NEW_TAB_FR_FR",
+                        "CORPUS_SLATE_CONFIGURATION_ID": "41081474-0b70-5cd8-8177-f495b371030b",
+                        "CORPUS_ITEM_ID": "eca15def-ec21-49c4-b828-5014815b1918",
+                        "TRAILING_1_DAY_IMPRESSIONS": 3487376,
+                        "TRAILING_1_DAY_OPENS": 17304,
+                        "TRAILING_7_DAY_IMPRESSIONS": 0,
+                        "TRAILING_7_DAY_OPENS": 0,
+                        "TRAILING_14_DAY_IMPRESSIONS": 0,
+                        "TRAILING_14_DAY_OPENS": 0,
+                        "TRAILING_21_DAY_IMPRESSIONS": 0,
+                        "TRAILING_21_DAY_OPENS": 0,
+                        "TRAILING_28_DAY_IMPRESSIONS": 0,
+                        "TRAILING_28_DAY_OPENS": 0,
+                    }
+                ]
+            ),
+            feature_group_name=FeatureGroupSettings().corpus_engagement_feature_group_name,
+        )
+    )
---
+++ data-products/tests/shared/test_feature_store.py
@@ -0,0 +1,126 @@
+from unittest import mock
+
+import aioboto3
+import pandas as pd
+import pytest
+import pytest_asyncio
+from botocore.stub import Stubber
+from common.settings import CommonSettings
+from prefect.testing.utilities import prefect_test_harness
+
+from shared.feature_store import (
+    dataframe_to_feature_group,
+    INGEST_ROWS_RETRIES,
+    ingest_row,
+)
+
+CS = CommonSettings()  # type: ignore
+
+
+@pytest.fixture()
+def df_features() -> pd.DataFrame:
+    return pd.DataFrame(
+        [{"id": "foobar-1", "clicks": 200}, {"id": "foobar-2", "clicks": 300}]
+    )
+
+
+@pytest.fixture()
+def features_row(df_features):
+    return list(df_features.itertuples(index=False))[0]
+
+
+@pytest.fixture()
+def features_row_expected_params(features_row):
+    return {
+        "FeatureGroupName": "my_feature_group",
+        "Record": [
+            {"FeatureName": "id", "ValueAsString": str(features_row.id)},
+            {"FeatureName": "clicks", "ValueAsString": str(features_row.clicks)},
+        ],
+    }
+
+
+@pytest_asyncio.fixture
+async def stubbed_featurestore(features_row_expected_params):
+    async with aioboto3.Session().client(
+        "sagemaker-featurestore-runtime"
+    ) as featurestore:
+        stubber = Stubber(featurestore)
+        stubber.add_response(
+            "put_record",
+            {},
+            expected_params=features_row_expected_params,
+        )
+        stubber.activate()
+
+        yield featurestore
+        stubber.assert_no_pending_responses()
+
+
+@pytest_asyncio.fixture
+async def stubbed_featurestore_error(features_row_expected_params):
+    async with aioboto3.Session().client(
+        "sagemaker-featurestore-runtime"
+    ) as featurestore:
+        stubber = Stubber(featurestore)
+        stubber.add_client_error(
+            "put_record",
+            service_error_code="ServiceUnavailable",
+            expected_params=features_row_expected_params,
+        )
+        stubber.activate()
+
+        yield featurestore
+        stubber.assert_no_pending_responses()
+
+
+@pytest.mark.asyncio
+async def test_ingest_row(features_row, stubbed_featurestore, caplog):
+    await ingest_row(
+        row=features_row,
+        feature_group_name="my_feature_group",
+        featurestore=stubbed_featurestore,
+    )
+
+
+@pytest.mark.asyncio
+async def test_ingest_row_exception(features_row, stubbed_featurestore_error, caplog):
+    with pytest.raises(stubbed_featurestore_error.exceptions.ServiceUnavailable):
+        await ingest_row(
+            row=features_row,
+            feature_group_name="my_feature_group",
+            featurestore=stubbed_featurestore_error,
+        )
+
+
+@pytest.mark.asyncio
+async def test_dataframe_to_feature_group(df_features, caplog):
+    with prefect_test_harness():
+        with mock.patch(
+            "shared.feature_store.ingest_row",
+            new_callable=mock.AsyncMock,
+            return_value=None,
+        ) as mock_ingest_row:
+            await dataframe_to_feature_group(
+                df_features, feature_group_name="my_feature_group"
+            )
+
+            assert mock_ingest_row.call_count == len(df_features)
+
+
+@pytest.mark.asyncio
+async def test_dataframe_to_feature_group_exception(df_features, caplog):
+    with prefect_test_harness():
+        with mock.patch(
+            "shared.feature_store.ingest_row", new_callable=mock.AsyncMock
+        ) as mock_ingest_row:
+            mock_ingest_row.side_effect = ValueError()
+            with pytest.raises(ValueError):
+                await dataframe_to_feature_group(
+                    df_features,
+                    feature_group_name="my_feature_group",
+                )
+
+            assert mock_ingest_row.call_count == (INGEST_ROWS_RETRIES + 1) * len(
+                df_features
+            )
