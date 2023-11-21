292a73d765c96d764d3d2c9bea0ba610c8dfb258
Mathijs Miermans <m.miermans@gmail.com>
2023-11-21 11:21:31 -0800

    feat(legacy_curated_feeds_flow): send SQS messages

--- data-products/src/shared/api_clients/sqs.py
+++ data-products/src/shared/api_clients/sqs.py
@@ -8,11 +8,12 @@ from typing import Union, Dict
 
 import boto3
 import prefect
-from prefect import task
+from prefect import task, get_run_logger
+from prefect.context import FlowRunContext
 from pydantic import BaseModel, root_validator
 from common.settings import CommonSettings
 
-CS = CommonSettings() # type: ignore
+CS = CommonSettings()  # type: ignore
 
 SQS_MESSAGE_VERSION = 3
 SQS_REC_QUEUE = (
@@ -31,6 +32,7 @@ ONE_WEEK_S = ONE_DAY_S * 7
 
 
 # todo: remove sqs when we finished migration to SageMaker Feature Store
+
 
 class RecommendationCandidate(BaseModel):
     item_id: int
@@ -64,7 +66,11 @@ class CandidateSet(BaseModel):
 
     @root_validator(pre=False)
     def validate_list_elements(cls, values):
-        expected_type = CurationProspect if values.get("type") == CandidateType.prospect else RecommendationCandidate
+        expected_type = (
+            CurationProspect
+            if values.get("type") == CandidateType.prospect
+            else RecommendationCandidate
+        )
         for c in values.get("candidates"):
             assert type(c) == expected_type, f"expected {expected_type} : got {type(c)}"
         return values
@@ -84,61 +90,76 @@ class SQSInfo:
 
 
 class SQSConfig:
-    recommendation = SQSInfo(name=CandidateType.recommendation,
-                             sqs_queue=SQS_REC_QUEUE)
-    prospecting = SQSInfo(name=CandidateType.prospect,
-                          sqs_queue=SQS_PROSPECT_QUEUE)
+    recommendation = SQSInfo(name=CandidateType.recommendation, sqs_queue=SQS_REC_QUEUE)
+    prospecting = SQSInfo(name=CandidateType.prospect, sqs_queue=SQS_PROSPECT_QUEUE)
 
 
 @task()
 def validate_candidate_items(corpus_items: List[Dict]):
     min_item_count = 1
-    expected_keys = ['ID', 'PUBLISHER']
+    expected_keys = ["ID", "PUBLISHER"]
 
     assert len(corpus_items) >= min_item_count
-    assert all(set(expected_keys).difference(set(corpus_item.keys())) == set() for corpus_item in corpus_items)
-    assert all(isinstance(corpus_item['ID'], int) and corpus_item['ID'] != None for corpus_item in corpus_items)
-    assert all(isinstance(corpus_item['PUBLISHER'], str) and corpus_item['PUBLISHER'] != '' for corpus_item in corpus_items)
+    assert all(
+        set(expected_keys).difference(set(corpus_item.keys())) == set()
+        for corpus_item in corpus_items
+    )
+    assert all(
+        isinstance(corpus_item["ID"], int) and corpus_item["ID"] != None
+        for corpus_item in corpus_items
+    )
+    assert all(
+        isinstance(corpus_item["PUBLISHER"], str) and corpus_item["PUBLISHER"] != ""
+        for corpus_item in corpus_items
+    )
 
     return corpus_items
 
 
 @task()
-def put_results(candidate_set_id: str,
-                candidates: Union[List[RecommendationCandidate], List[CurationProspect]],
-                expires_in_s: int = ONE_WEEK_S * 4,
-                sqs_queue_name: str = SQSConfig.recommendation.sqs_queue,
-                curated: bool = False,
-                candidate_type: CandidateType = CandidateType.recommendation):
+def put_results(
+    candidate_set_id: str,
+    candidates: Union[List[RecommendationCandidate], List[CurationProspect]],
+    expires_in_s: int = ONE_WEEK_S * 4,
+    sqs_queue_name: str = SQSConfig.recommendation.sqs_queue,
+    curated: bool = False,
+    candidate_type: CandidateType = CandidateType.recommendation,
+):
     # Check that the candidate_set_id is actually a GUID
     uuid.UUID(candidate_set_id)
 
     # curated items are required to have a feed_id, these are generally optional
     if curated:
         for c in candidates:
-            assert type(c.feed_id) == int, f"{c.item_id} has feed_id={c.feed_id} in a curated flow"
+            assert (
+                type(c.feed_id) == int
+            ), f"{c.item_id} has feed_id={c.feed_id} in a curated flow"
+
+    flow_run = FlowRunContext.get().flow_run.dict()
 
     # note there are extra fields in this model not present in the recommendation-api
     # candidate set model, however they should pass through
-    candidate_set = CandidateSet(id=candidate_set_id,
-                                 flow=prefect.context.flow_name,
-                                 run=prefect.context.flow_run_id,
-                                 expires_at=int(time.time() + expires_in_s),
-                                 type=candidate_type,
-                                 version=SQS_MESSAGE_VERSION,
-                                 candidates=candidates)
+    candidate_set = CandidateSet(
+        id=candidate_set_id,
+        flow=flow_run['name'],
+        run=str(flow_run['id']),
+        expires_at=int(time.time() + expires_in_s),
+        type=candidate_type,
+        version=SQS_MESSAGE_VERSION,
+        candidates=candidates,
+    )
 
-    logger = prefect.context.get("logger")
+    logger = get_run_logger()
     message = json.dumps(candidate_set.dict())
-    logger.info(f'SQS message: {message}')
+    logger.info(f"SQS message: {message}")
 
     if expires_in_s <= 0:
         raise AssertionError(f"Invalid expiry set: {expires_in_s=}")
 
     message = json.dumps(candidate_set.dict())
     boto3_session = boto3.session.Session()  # Each thread needs its own boto3 session
-    sqs = boto3_session.resource('sqs')
+    sqs = boto3_session.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
-    logger.info(f'Sent {len(candidates)} candidates from {candidate_set_id}')
+    logger.info(f"Sent {len(candidates)} candidates from {candidate_set_id}")
     sqs_result = queue.send_message(MessageBody=message)
-    logger.info(f'SQS result: {sqs_result}')
+    logger.info(f"SQS result: {sqs_result}")
