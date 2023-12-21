5b2de1e8ae57722ef2ee81a3e335f326ba3bd91f
Braun <breyes@mozilla.com>
2023-12-21 11:06:16 -0600

    feat(freestar): final commits for refactor of freestar flow

--- data-products/src/freestar_revenue_reporting/freestar_reporting_flow.py
+++ data-products/src/freestar_revenue_reporting/freestar_reporting_flow.py
@@ -1,34 +1,63 @@
-import json
 import os
-import time
+from datetime import date, datetime, time
 from pathlib import Path
 
 import pandas as pd
+import pendulum
 import requests
 from common.databases.snowflake_utils import MozSnowflakeConnector
-from common.deployment import FlowDeployment, FlowEnvar, FlowSpec
-from common.settings import CommonSettings
+from common.deployment.worker import FlowDeployment, FlowSpec
+from common.settings import CommonSettings, NestedSettings, SecretSettings
 from prefect import flow, get_run_logger, task
-from prefect.server.schemas.schedules import CronSchedule
 from prefect_snowflake.database import snowflake_multiquery, snowflake_query_sync
+from pydantic import BaseModel
+from shared.async_utils import process_parallel_subflows_task
 
 CS = CommonSettings()  # type: ignore
 
+
+# creating settings objects to leverage new secrets logic
+class FreestarCredentials(NestedSettings):
+    api_key: str
+
+
+class FreestarSettings(SecretSettings):
+    freestar_credentials: FreestarCredentials
+
+
+# reusable date format
+DATE_FMT = "YYYY-MM-DD"
+
 # Define the output Parquet file template
-output_parquet_path = "/tmp/freestar"
-output_parquet_filename = os.path.join(output_parquet_path, "data_{0}.parquet")
+OUTPUT_PARQUET_PATH = "/tmp/freestar"
 
 
-@task(retries=5, retry_delay_seconds=5)
-def extract_freestar_data():
+def make_file_path(report_date: str) -> str:
+    """Produce full file path template based on report_date.
+
+    Args:
+        report_date (str): Report date to add to path.
+
+    Returns:
+        str: Path string
+    """
+    return os.path.join(OUTPUT_PARQUET_PATH, report_date, "data_{0}.parquet")
+
+
+@task(retries=5, retry_delay_seconds=5, task_run_name="extract-api-data-{report_date}")
+def extract_freestar_data(report_date: str, api_key: str, record_limit: int):
+    """Task for extracting data and loading to file.
+
+    Args:
+        report_date (str): Report date to run for.
+        api_key (str): API key from settings.
+        record_limit (int): Record limit to use.
+    """
     logger = get_run_logger()
-    # Define the API base URL and endpoint
-    API_BASE_URL = "https://analytics.pub.network"
-    API_ENDPOINT = "/cubejs-api/v1/load"
-    FREESTAR_API_KEY = json.loads(os.environ["FREESTAR_CREDENTIALS"])["api_key"]
 
-    # Retrieve 2,000 records at a time to accomodate 5 second timeout
-    record_limit = 2000
+    # Define the API base URL and endpoint
+    api_base_url = "https://analytics.pub.network"
+    api_endpoint = "/cubejs-api/v1/load"
 
     # Define the request payload with initial pagination parameters
     request_payload = {
@@ -60,17 +89,16 @@ def extract_freestar_data():
             "timeDimensions": [
                 {
                     "dimension": "NdrPrebid.record_date",
-                    "dateRange": "Yesterday",  # for backfill use "dateRange": ["2023-09-01", "2023-09-08"]
+                    "dateRange": [report_date, report_date],
                 }
             ],
-            "limit": record_limit,  # Retrieve 2,000 records at a time to accomodate 5 second timeout
+            "limit": record_limit,
             "offset": 0,  # Start with an offset of 0
         }
     }
-
     # Set up the headers with the API token
     headers = {
-        "Authorization": f"Bearer {FREESTAR_API_KEY}",
+        "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json",
     }
 
@@ -78,12 +106,14 @@ def extract_freestar_data():
     total_count = 0  # Initialize the total count tracker
 
     # create directory
-    Path(output_parquet_path).mkdir(parents=True, exist_ok=True)
+    file_path = make_file_path(report_date)
+    file_path_obj = Path(file_path)
+    file_path_obj.parent.mkdir(parents=True, exist_ok=True)
 
     while True:
         # Send the POST request to the API with the current pagination parameters
         response = requests.post(
-            API_BASE_URL + API_ENDPOINT, json=request_payload, headers=headers
+            api_base_url + api_endpoint, json=request_payload, headers=headers
         )
         response.raise_for_status()  # Raise an exception for HTTP errors
 
@@ -104,7 +134,7 @@ def extract_freestar_data():
         data = response_json["data"]
         record_count = len(data)
         df = pd.DataFrame(data)
-        df.to_parquet(output_parquet_filename.format(page_number))
+        df.to_parquet(file_path.format(page_number))
         total_count += record_count
 
         # Check if there are more records to retrieve
@@ -115,14 +145,16 @@ def extract_freestar_data():
         request_payload["query"]["offset"] += record_limit
         page_number += 1
 
-        # Add a 5-second delay before fetching the next page to accomodate
-        # Freestar's API limits
-        time.sleep(5)
-
     logger.info(f"Total records retrieved: {total_count}")
 
 
-table_schema = """
+# SQL for getting dates currently in Snowflake
+ACTUAL_DATES = """select distinct RECORD_DATE
+    from freestar.freestar_daily_extracts
+    where RECORD_DATE >= %(start_date)s
+    and RECORD_DATE <= %(end_date)s"""
+
+TABLE_SCHEMA = """
     record_date DATE,
     network STRING,
     url STRING,
@@ -145,141 +177,244 @@ table_schema = """
 """
 
 # Define table to load
-snowflake_table = "freestar_daily_extracts"
+SNOWFLAKE_TABLE = "freestar_daily_extracts"
 
 # Define SQL statements
-create_schema_sql = """
+CREATE_SCHEMA_SQL = """
 CREATE SCHEMA IF NOT EXISTS freestar;
 """
 
-create_table_sql = f"""
-CREATE TABLE IF NOT EXISTS freestar.{snowflake_table} (
-    {table_schema}
+CREATE_TABLE_SQL = f"""
+CREATE TABLE IF NOT EXISTS freestar.{SNOWFLAKE_TABLE} (
+    {TABLE_SCHEMA}
 );
 """
 
-format_file_sql = f"""
-CREATE OR REPLACE FILE FORMAT freestar.{snowflake_table}_format
+FORMAT_FILE_SQL = f"""
+CREATE OR REPLACE FILE FORMAT freestar.{SNOWFLAKE_TABLE}_format
 TYPE = parquet;
 """
 
-create_stage_sql = f"""
-CREATE OR REPLACE STAGE freestar.{snowflake_table}_stage
-FILE_FORMAT = freestar.{snowflake_table}_format;
-"""
-
-put_parquet_sql = f"""
-PUT file://{output_parquet_filename.format('*')} @freestar.{snowflake_table}_stage
-"""
-
-create_temp_table = f"""
-CREATE TEMPORARY TABLE freestar.{snowflake_table}_tmp (
-    {table_schema}
-);
-"""
-
-load_sql = f"""
-COPY INTO freestar.{snowflake_table}_tmp
-    FROM (SELECT $1:record_date::DATE,
-                 $1:network::STRING,
-                 $1:url::STRING,
-                 $1:utm_campaign::STRING,
-                 $1:utm_content::STRING,
-                 $1:utm_medium::STRING,
-                 $1:utm_source::STRING,
-                 $1:utm_term::STRING,
-                 $1:site_domain::STRING,
-                 $1:ad_unit::STRING,
-                 $1:size::STRING,
-                 $1:country_code::STRING,
-                 $1:device_type::STRING,
-                 $1:device_os::STRING,
-                 $1:browser::STRING,
-                 $1:integration_partner::STRING,
-                 $1:impressions::INTEGER,
-                 $1:net_revenue::INTEGER,
-                 $1:net_cpm::FLOAT
-        FROM @freestar.{snowflake_table}_stage
-        );
-"""
-
-begin_transaction = """
-BEGIN TRANSACTION;
-"""
-
-delete_sql = f"""
-DELETE FROM freestar.{snowflake_table}
-    WHERE record_date IN (SELECT record_date FROM freestar.{snowflake_table}_tmp);
-"""
-
-insert_sql = f"""
-INSERT INTO freestar.{snowflake_table}
-    SELECT * FROM freestar.{snowflake_table}_tmp;
+CREATE_STAGE_SQL = f"""
+CREATE OR REPLACE STAGE freestar.{SNOWFLAKE_TABLE}_stage
+FILE_FORMAT = freestar.{SNOWFLAKE_TABLE}_format;
 """
 
 
-# Define the Prefect flow
-@flow(name="Freestar Report Flow")
-async def freestar_report_flow():
-    eft = extract_freestar_data()
-    create_schema = await snowflake_query_sync(
-        query=create_schema_sql,
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[eft],
-    )  # type: ignore
-    create = await snowflake_query_sync(
-        query=create_table_sql,
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[create_schema],
-    )  # type: ignore
-    format_file = await snowflake_query_sync(
-        query=format_file_sql,
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[create],
-    )  # type: ignore
-    create_stage = await snowflake_query_sync(
-        query=create_stage_sql,
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[format_file],
-    )  # type: ignore
+@flow(
+    name="ingest-freestar-data-subflow",
+    flow_run_name="freestar-ingestion-{report_date}",
+)
+async def ingest_freestar_data_subflow(
+    report_date: str,
+    sf_connector: MozSnowflakeConnector,
+    api_key: str,
+    record_limit: int,
+):
+    """Subflow for loading parquet files into Snowflake.
+
+    Args:
+        report_date (str): Report date to run for.
+        sf_connector (MozSnowflakeConnector): Connector block to use for tasks.
+        api_key (str): API key from settings.
+        record_limit (int): Record limit to use.
+    """
+    tmp_table_name = f"{SNOWFLAKE_TABLE}_{report_date.replace('-', '')}_tmp"
+
+    file_path = make_file_path(report_date)
+
+    put_parquet_sql = f"""
+    PUT file://{file_path.format('*')} @freestar.{SNOWFLAKE_TABLE}_stage/{report_date}
+    """
+
+    create_temp_table = f"""
+    CREATE TEMPORARY TABLE freestar.{tmp_table_name} (
+        {TABLE_SCHEMA}
+    );
+    """
+
+    load_sql = f"""
+    COPY INTO freestar.{tmp_table_name}
+        FROM (SELECT $1:record_date::DATE,
+                    $1:network::STRING,
+                    $1:url::STRING,
+                    $1:utm_campaign::STRING,
+                    $1:utm_content::STRING,
+                    $1:utm_medium::STRING,
+                    $1:utm_source::STRING,
+                    $1:utm_term::STRING,
+                    $1:site_domain::STRING,
+                    $1:ad_unit::STRING,
+                    $1:size::STRING,
+                    $1:country_code::STRING,
+                    $1:device_type::STRING,
+                    $1:device_os::STRING,
+                    $1:browser::STRING,
+                    $1:integration_partner::STRING,
+                    $1:impressions::INTEGER,
+                    $1:net_revenue::INTEGER,
+                    $1:net_cpm::FLOAT
+            FROM @freestar.{SNOWFLAKE_TABLE}_stage/{report_date}
+            );
+    """
+
+    begin_transaction = """
+    BEGIN TRANSACTION;
+    """
+
+    delete_sql = f"""
+    DELETE FROM freestar.{SNOWFLAKE_TABLE}
+        WHERE record_date IN (SELECT record_date FROM freestar.{tmp_table_name});
+    """
+
+    insert_sql = f"""
+    INSERT INTO freestar.{SNOWFLAKE_TABLE}
+        SELECT * FROM freestar.{tmp_table_name};
+    """
+
+    delete_tmp_table_sql = f"""
+    DROP TABLE freestar.{tmp_table_name};
+    """
+
+    sfc = sf_connector
+    eft = extract_freestar_data.with_options(
+        task_run_name=f"load-api-data-files-{report_date}"
+    )(report_date, api_key, record_limit)
     put_parquet = await snowflake_query_sync(
         query=put_parquet_sql,
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[create_stage],
-    )  # type: ignore
-    await snowflake_multiquery(
+        snowflake_connector=sfc,
+        wait_for=[eft],  # type: ignore
+    )
+    await snowflake_multiquery.with_options(
+        task_run_name=f"load-api-data-to-table-{report_date}"
+    )(
         queries=[
             create_temp_table,
             load_sql,
             begin_transaction,
             delete_sql,
             insert_sql,
+            delete_tmp_table_sql,
         ],
-        snowflake_connector=MozSnowflakeConnector(),
-        wait_for=[put_parquet],
+        snowflake_connector=sfc,
+        wait_for=[put_parquet],  # type: ignore
         as_transaction=True,
-    )  # type: ignore
+    )
+
+
+class FlowDateInputs(BaseModel):
+    """Model to represent flow inputs.
+    This also make fields optional to enable default logic.
+    """
+
+    start_date: date | None = None
+    end_date: date | None = None
+    overwrite: bool = False
+    record_limit: int = 50000
+
+
+# Define the Prefect flow
+@flow(name="Freestar Report Flow")
+async def freestar_report_flow(dates: FlowDateInputs = FlowDateInputs()):
+    """Main flow that will bootstrap Snowflake objects as needed.
+    This will also do the necessary evaluation of dates to determine
+    what needs to be processed.  This include handling an overwrite and backfill.
+
+    Args:
+        dates (FlowDateInputs, optional): Flow inputs. Defaults to FlowDateInputs().
+    """
+
+    # create connector to pass on
+    sfc = MozSnowflakeConnector()
+
+    async def get_dates(
+        start_date: date | None = None,
+        end_date: date | None = None,
+    ) -> list[str]:
+        """Nested function handle date evaluation
+
+        Args:
+            start_date (date | None, optional): Explicit value to use. Defaults to None.
+            end_date (date | None, optional): Explicit value to use. Defaults to None.
+
+        Returns:
+            list: list of date strings.
+        """
+
+        # default date logic for regular runs
+        if not start_date:
+            start_date = pendulum.now().subtract(days=14)
+        else:
+            start_date = pendulum.instance(datetime.combine(start_date, time(0, 0, 0)))
+
+        if not end_date:
+            end_date = pendulum.now().subtract(days=1)
+        else:
+            end_date = pendulum.instance(datetime.combine(end_date, time(0, 0, 0)))
+
+        # logic to determine base set if dates to process
+        dt_period = end_date - start_date
+        dt_period.in_days()  # type: ignore
+
+        # logic to produce final dates to process using dates already
+        # processed if enabled
+        expected_days = [d.date() for d in dt_period]  # type: ignore
+        dates_to_process = sorted(set(expected_days))
+        if not dates.overwrite:
+            actual_dates = await snowflake_query_sync(
+                query=ACTUAL_DATES,
+                snowflake_connector=sfc,
+                params={"start_date": start_date, "end_date": end_date},
+            )  # type: ignore
+
+            actual_days = [x[0] for x in actual_dates]
+            dates_to_process = sorted(set(expected_days).difference(set(actual_days)))
+
+        return [i.format(DATE_FMT) for i in list(dates_to_process)]
+
+    # bootstrap tables
+    create_schema = await snowflake_query_sync(
+        query=CREATE_SCHEMA_SQL,
+        snowflake_connector=sfc,
+    )
+    create = await snowflake_query_sync(
+        query=CREATE_TABLE_SQL,
+        snowflake_connector=sfc,
+        wait_for=[create_schema],  # type: ignore
+    )
+    format_file = await snowflake_query_sync(
+        query=FORMAT_FILE_SQL,
+        snowflake_connector=sfc,
+        wait_for=[create],  # type: ignore
+    )
+    create_stage = await snowflake_query_sync(
+        query=CREATE_STAGE_SQL,
+        snowflake_connector=sfc,
+        wait_for=[format_file],  # type: ignore
+    )
+
+    # Fetch credentials
+    freestar_creds = FreestarSettings()  # type: ignore
+
+    # create and submit subflows
+    jobs = [
+        ingest_freestar_data_subflow(
+            j, sfc, freestar_creds.freestar_credentials.api_key, dates.record_limit
+        )
+        for j in await get_dates(start_date=dates.start_date, end_date=dates.end_date)
+    ]
+    await process_parallel_subflows_task(jobs, wait_for=[create_stage])  # type: ignore
 
 
 FLOW_SPEC = FlowSpec(
     flow=freestar_report_flow,
     docker_env="base",
-    secrets=[
-        FlowEnvar(
-            envar_name="DF_CONFIG_SNOWFLAKE_CREDENTIALS",
-            envar_value=f"data-flows/{CS.deployment_type}/snowflake-credentials",
-        ),
-        FlowEnvar(
-            envar_name="FREESTAR_CREDENTIALS",
-            envar_value=f"data-flows/{CS.deployment_type}/freestar-credentials",
-        ),
-    ],
     deployments=[
         FlowDeployment(
-            deployment_name="freestar_extraction",
+            name="freestar_extraction",
             # Running at 10 a.m. UTC to ensure it runs after midnight
             # since we are pulling from Freestar's "Yesterday"
-            schedule=CronSchedule(cron="0 10 * * *"),
+            cron="0 10 * * *",
         ),
     ],
 )
@@ -287,4 +422,4 @@ FLOW_SPEC = FlowSpec(
 if __name__ == "__main__":
     from asyncio import run
 
-    run(freestar_report_flow())  # type: ignore
+    run(freestar_report_flow())
--- data-products/src/shared/async_utils.py
+++ data-products/src/shared/async_utils.py
@@ -1,10 +1,18 @@
 import asyncio
 from typing import Coroutine, Sequence
 
+from prefect import task
+
 
 async def process_parallel_subflows(
     coroutines: Sequence[Coroutine], concurrency_limit: int = 10
 ):
+    """Helper function to submit async subflow with managed concurrency.
+
+    Args:
+        coroutines (Sequence[Coroutine]): List of coroutines
+        concurrency_limit (int, optional): Explicit value to use. Defaults to 10.
+    """
     semaphore = asyncio.Semaphore(concurrency_limit)
 
     async def with_concurrency_limit(coroutine: Coroutine) -> Coroutine:
@@ -14,3 +22,16 @@ async def process_parallel_subflows(
     group = [with_concurrency_limit(coroutine) for coroutine in coroutines]
 
     await asyncio.gather(*group, return_exceptions=True)
+
+
+@task()
+async def process_parallel_subflows_task(
+    coroutines: Sequence[Coroutine], concurrency_limit: int = 10
+):
+    """Task to manage subflow submission.
+
+    Args:
+        coroutines (Sequence[Coroutine]): List of coroutines
+        concurrency_limit (int, optional): Explicit value to use. Defaults to 10.
+    """
+    await process_parallel_subflows(coroutines, concurrency_limit)
