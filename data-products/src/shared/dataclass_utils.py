31303e5436015a8bd8ae7e43acdf199e643ffe9f
Braun <breyes@mozilla.com>
2024-01-12 11:09:01 -0600

    feat(braze): changes to support migration of braze update flow to v2

---
+++ data-products/src/shared/dataclass_utils.py
@@ -0,0 +1,33 @@
+import dataclasses
+import json
+from typing import Any
+
+
+class DataClassJSONEncoderWithoutNoneValues(json.JSONEncoder):
+    """
+    Allows a dataclass to be encoded as json using json.dumps(foo, cls=DataClassJSONEncoder)
+    Fields set to `Missing()` will not be present in the output.
+    """
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            d = dataclasses.asdict(o)
+            return _without_none_values(d)
+        return super().default(o) # pragma: no cover
+
+
+def _without_none_values(obj: Any):
+    """
+    Returns copy of input with all None values removed, even recursively in sequences.
+
+    Code is based on a Stackoverflow answer: https://stackoverflow.com/a/20558778/331030, but unlike this answer,
+    keys that are None are not removed from dictionaries, only None values are.
+
+    :param obj: list, tuple, set, dict, or primitive types (str, int, float, etc.)
+    :return: A copy of obj without any None values, including recursively into list, tuple, set, dict.
+    """
+    if isinstance(obj, (list, tuple, set)):
+        return type(obj)(_without_none_values(x) for x in obj if x is not None)
+    elif isinstance(obj, dict):
+        return {k: _without_none_values(v) for k, v in obj.items() if v is not None}
+    else:
+        return obj
---
+++ data-products/src/shared/tasks.py
@@ -0,0 +1,27 @@
+from typing import Dict, List, Sequence, Tuple, TypeVar
+
+from prefect import task
+from shared.iteration_utils import chunks
+
+T = TypeVar("T")
+
+
+@task()
+def split_dict_of_lists_in_chunks(
+    d: Dict[str, List[T]], chunk_size: int
+) -> List[Tuple[str, List[T]]]:
+    result = []
+    for k, v_list in d.items():
+        result.extend((k, chunk) for chunk in chunks(v_list, n=chunk_size))
+    return result
+
+
+@task()
+def split_in_chunks(sequence: Sequence[T], chunk_size: int) -> List[Sequence[T]]:
+    """
+    Splits the input into chunks.
+    :param sequence: List or other type of sequence to split into chunks
+    :param chunk_size: Maximum size of a chunk.
+    :return: List of list, where the inner list is a chunk of user deltas of at most _chunk_size_ items.
+    """
+    return list(chunks(sequence, n=chunk_size))
