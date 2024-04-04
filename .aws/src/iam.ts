24eeafacd81870b87fdd1b47e809a045461aadea
Braun <breyes@mozilla.com>
2024-04-04 13:28:54 -0500

    feat(aws): removing prefect agent service

--- .aws/src/iam.ts
+++ .aws/src/iam.ts
@@ -347,12 +347,10 @@ export class DataFlowsIamRoles extends Construct {
       ]
     };
   }
-  // Give access to update 
+  // Give access to update
   private updateSecrets(): DataAwsIamPolicyDocumentStatement {
     return {
-      actions: [
-        'secretsmanager:UpdateSecret'
-      ],
+      actions: ['secretsmanager:UpdateSecret'],
       effect: 'Allow',
       resources: [
         `arn:aws:secretsmanager:${this.region.name}:${this.caller.accountId}:secret:data-flows/${this.deploymentType}/freestar-credentials`
