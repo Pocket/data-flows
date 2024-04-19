77a96943cb874e83c0e2c4b291670e83ba2c12e3
Braun <breyes@mozilla.com>
2024-04-19 14:22:19 -0500

    fix: fixing iam perms

--- .aws/src/iam.ts
+++ .aws/src/iam.ts
@@ -351,7 +351,11 @@ export class DataFlowsIamRoles extends Construct {
   // Give access to update
   private updateSecrets(): DataAwsIamPolicyDocumentStatement {
     return {
-      actions: ['secretsmanager:UpdateSecret'],
+      actions: [
+        'secretsmanager:UpdateSecret',
+        'kms:GenerateDataKey',
+        'kms:Encrypt'
+      ],
       effect: 'Allow',
       resources: [
         `arn:aws:secretsmanager:${this.region.name}:${this.caller.accountId}:secret:data-flows/${this.deploymentType}/freestar-credentials`
