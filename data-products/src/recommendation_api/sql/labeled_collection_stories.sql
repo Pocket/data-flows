b86cc0baf5943ac3c7cea0fe3e3522bea951673c
Daniel Brooks <bass_rock@me.com>
2024-05-28 07:49:50 -0700

    fix(labeled-collections): ensuring that the stories in collection slates always have a topic and publisher

--- data-products/src/recommendation_api/sql/labeled_collection_stories.sql
+++ data-products/src/recommendation_api/sql/labeled_collection_stories.sql
@@ -18,3 +18,4 @@ select
 from  analytics.dbt.approved_corpus_items as aci
 inner join labeled_stories as ls on ls.url = aci.url
 where aci.approved_corpus_item_external_id != 'c931d2f5-0205-48f1-a773-dd0e682977b1'   -- See #incidents on 2023-03-21
+and TOPIC is not null and PUBLISHER is not null -- Recommendations need to always have a topic and publisher
