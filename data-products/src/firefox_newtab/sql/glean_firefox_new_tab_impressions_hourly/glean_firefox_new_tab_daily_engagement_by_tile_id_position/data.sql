5dea56848f3c07ea49d0f8b09288d0bddfcd503f
Chelsey Beck <cbeck@mozilla.com>
2024-09-12 13:36:23 -0700

    filtering out records where scheduled corpus item id is null

--- data-products/src/firefox_newtab/sql/glean_firefox_new_tab_impressions_hourly/glean_firefox_new_tab_daily_engagement_by_tile_id_position/data.sql
+++ data-products/src/firefox_newtab/sql/glean_firefox_new_tab_impressions_hourly/glean_firefox_new_tab_daily_engagement_by_tile_id_position/data.sql
@@ -48,6 +48,9 @@ WITH
         'recommendation_id') IS NOT NULL
       OR mozfun.map.get_key(e.extra,
         'tile_id') IS NOT NULL)
+    --filter out records where scheduled corpus item ID is populated to prevent double count
+    AND mozfun.map.get_key(e.extra,
+        'scheduled_corpus_item_id') IS NULL
     --include only data from Firefox 121+
     AND SAFE_CAST(SPLIT(client_info.app_display_version, '.')[0] AS int64) >= 121 )
 SELECT
