f1b0f0a4692e296ca7804cbea7af50e5a118d6cb
Chelsey Beck <64881557+chelseybeck@users.noreply.github.com>
2025-01-15 09:03:20 -0800

    Update data-products/src/firefox_newtab/sql/ads_impressions_hourly/ads_daily_engagement_by_tile_id_position/data.sql
    updating logic to accommodate both live and stable tables depending on backfill
    
    Co-authored-by: Curtis Morales <curtismorales@gmail.com>

--- data-products/src/firefox_newtab/sql/ads_impressions_hourly/ads_daily_engagement_by_tile_id_position/data.sql
+++ data-products/src/firefox_newtab/sql/ads_impressions_hourly/ads_daily_engagement_by_tile_id_position/data.sql
@@ -1,25 +1,47 @@
-SELECT
-  DATE(i.submission_timestamp) AS happened_at,
-  NULL AS recommendation_id,
-  COALESCE(SAFE_CAST(i.metrics.string.ad_id AS int), -1) AS tile_id,
-  COALESCE(SAFE_CAST(i.metrics.string.ad_client_position AS int), -1) AS position,
-  SUM(CASE
-      WHEN i.metrics.string.ad_interaction = 'impression' THEN 1
-      ELSE 0
-  END
-    ) AS impression_count,
-  SUM(CASE
-      WHEN i.metrics.string.ad_interaction = 'click' THEN 1
-      ELSE 0
-  END
-    ) AS click_count,
-  0 AS save_count,
-  0 AS dismiss_count
-FROM
-  ads_backend.interaction AS i
-WHERE 
+WITH uapi_interactions_deduped AS (
+  SELECT
+    DATE(submission_timestamp) AS happened_at,
+    NULL AS recommendation_id,
+    COALESCE(SAFE_CAST(metrics.string.ad_id AS int), -1) AS ad_id,
+    COALESCE(SAFE_CAST(metrics.string.ad_client_position AS int), -1) AS position,
+    metrics.string.ad_interaction AS interaction_type,
+  FROM
+    {% if with_stable %}
+    `moz-fx-data-shared-prod.ads_backend_stable.interaction_v1` AS i
+    {% else %}
+    `moz-fx-data-shared-prod.ads_backend_live.interaction_v1` AS i
+    {% endif %}
+  WHERE 
     submission_timestamp >= '{{ batch_start }}'
     AND submission_timestamp < '{{ batch_end }}'
+    AND metrics.string.ad_client_form_factor = 'desktop'
+    AND metrics.string.ad_client_placement = 'newtab_spocs'
+  QUALIFY 
+    ROW_NUMBER() OVER (
+      PARTITION BY DATE(submission_timestamp), document_id
+      ORDER BY submission_timestamp DESC
+  ) = 1
+)
+SELECT
+  happened_at,
+  recommendation_id,
+  ad_id,
+  position,
+  SUM(
+    CASE
+      WHEN interaction_type = 'impression' THEN 1
+      ELSE 0
+    END
+  ) AS impression_count,
+  SUM(
+    CASE
+      WHEN interaction_type = 'click' THEN 1
+      ELSE 0
+    END
+  ) AS click_count,
+  0 AS save_count,
+  0 AS dismiss_count,
+FROM uapi_interactions_deduped
 GROUP BY
   1,
   2,
